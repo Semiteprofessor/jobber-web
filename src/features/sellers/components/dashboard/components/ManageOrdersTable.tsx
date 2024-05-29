04f5c6deb3a5ca67838919392857be077e52279e
Taiwo Olapade <semiteprofessor@gmail.com>
2024-05-29 22:35:11 +0100

    Commit

--- src/features/sellers/components/dashboard/components/ManageOrdersTable.tsx
+++ src/features/sellers/components/dashboard/components/ManageOrdersTable.tsx
@@ -37,7 +37,35 @@ const ManageOrdersTable: FC<IOrderTableProps> = ({ type, orders, orderTypes }): 
     }
   };
 
-  return <div>ManageOrdersTable</div>;
+  return (
+    <>
+      {showCancelModal && (
+        <ApprovalModal approvalModalContent={approvalModalContent} onClose={() => setShowCancelModal(false)} onClick={onCancelOrder} />
+      )}
+      <div className="flex flex-col">
+        <div className="border-grey border border-b-0 px-3 py-3">
+          <div className="text-xs font-bold uppercase sm:text-sm md:text-base">{type} orders </div>
+        </div>
+        <table className="border-grey flex-no-wrap flex w-full table-auto flex-row overflow-hidden border text-sm text-gray-500 sm:inline-table">
+          {orderTypes > 0 ? (
+            <>
+              <thead className="border-grey border-b text-xs uppercase text-gray-700 sm:[&>*:not(:first-child)]:hidden">
+                {orders.map(() => (
+                  <tr
+                    key={uuidv4()}
+                    className="mb-1 flex flex-col flex-nowrap bg-sky-500 text-white sm:mb-0 sm:table-row md:table-row lg:bg-transparent lg:text-black"
+                  >
+                    <th className="p-3 text-center w-auto"></th>
+                    <th className="p-3 text-left w-auto">Buyer</th>
+                    <th className="p-3 text-left">Gig</th>
+                    <th className="p-3 text-center">{type === 'cancelled' ? 'Cancelled On' : 'Due On'}</th>
+                    {type === 'completed' && <th className="p-3 text-center">Delivered At</th>}
+                    <th className="p-3 text-center">Total</th>
+                    <th className="p-3 text-center">Status</th>
+                    {type === 'active' && <th className="p-3 text-center">Cancel</th>}
+                  </tr>
+                ))}
+              </thead></div>;
 };
 
 export default ManageOrdersTable;
