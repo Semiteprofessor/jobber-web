d9500be5d748c3cc83fedc64f41414f9ebac8116
Taiwo Olapade <semiteprofessor@gmail.com>
2024-05-29 22:34:29 +0100

    Commit

--- src/features/sellers/components/dashboard/components/ManageOrdersTable.tsx
+++ src/features/sellers/components/dashboard/components/ManageOrdersTable.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { FC, ReactElement, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { IOrderDocument, IOrderMessage, IOrderTableProps } from 'src/features/order/interfaces/order.interface';
+import { useCancelOrderMutation } from 'src/features/order/services/order.service';
+import Button from 'src/shared/button/Button';
+import { updateHeader } from 'src/shared/header/reducers/header.reducer';
+import ApprovalModal from 'src/shared/modals/ApprovalModal';
+import { IApprovalModalContent } from 'src/shared/modals/interfaces/modal.interface';
+import { TimeAgo } from 'src/shared/utils/timeago.utils';
+import { lowerCase, showErrorToast, showSuccessToast } from 'src/shared/utils/utils.service';
+import { useAppDispatch } from 'src/store/store';
+import { v4 as uuidv4 } from 'uuid';
 
-const ManageOrdersTable = () => {
-  return (
-    <div>ManageOrdersTable</div>
-  )
-}
+const ManageOrdersTable: FC<IOrderTableProps> = ({ type, orders, orderTypes }): ReactElement => {
+  const dispatch = useAppDispatch();
+  const [approvalModalContent, setApprovalModalContent] = useState<IApprovalModalContent>();
+  const [showCancelModal, setShowCancelModal] = useState<boolean>(false);
+  const selectedOrder = useRef<IOrderDocument>();
+  const [cancelOrder] = useCancelOrderMutation();
 
-export default ManageOrdersTable
+  const onCancelOrder = async (): Promise<void> => {
+    try {
+      const orderData: IOrderMessage = {
+        sellerId: `${selectedOrder.current?.sellerId}`,
+        buyerId: `${selectedOrder.current?.buyerId}`,
+        purchasedGigs: selectedOrder.current?.gigId
+      };
+      setShowCancelModal(false);
+      await cancelOrder({
+        paymentIntentId: `${selectedOrder.current?.paymentIntent}`,
+        orderId: `${selectedOrder.current?.orderId}`,
+        body: orderData
+      });
+      showSuccessToast('Order cancelled successfully.');
+    } catch (error) {
+      showErrorToast('Error cancelling order. Try again.');
+    }
+  };
+
+  return <div>ManageOrdersTable</div>;
+};
+
+export default ManageOrdersTable;
