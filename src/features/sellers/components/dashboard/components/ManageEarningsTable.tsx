3d20da7afcfe5a69e9736871da186d195378780b
Taiwo Olapade <semiteprofessor@gmail.com>
2024-06-01 22:49:26 +0100

    Commit

--- src/features/sellers/components/dashboard/components/ManageEarningsTable.tsx
+++ src/features/sellers/components/dashboard/components/ManageEarningsTable.tsx
@@ -9,7 +9,29 @@ import { v4 as uuidv4 } from 'uuid';
 const ManageEarningsTable: FC<IOrderTableProps> = ({ type, orders, orderTypes }): ReactElement => {
   const dispatch = useAppDispatch();
 
-  return <div>ManageEarningsTable</div>;
+  return (
+    <div className="flex flex-col justify-between">
+      <div className="border-grey border border-b-0 px-3 py-3">
+        <div className="font-bold uppercase text-xs sm:text-sm md:text-base">Payouts </div>
+      </div>
+      <table className="border border-grey w-full table-auto flex flex-row flex-no-wrap text-sm text-gray-500 overflow-hidden sm:inline-table">
+        {orderTypes > 0 ? (
+          <>
+            <thead className="border-grey border-b text-xs uppercase text-gray-700 sm:[&>*:not(:first-child)]:hidden">
+              {orders.map(() => (
+                <tr
+                  key={uuidv4()}
+                  className="bg-sky-500 text-white flex flex-col flex-nowrap sm:table-row md:table-row mb-1 sm:mb-0 lg:bg-transparent lg:text-black "
+                >
+                  <th className="p-3 text-left md:text-center">Date</th>
+                  <th className="p-3 text-left md:text-center">Activity</th>
+                  <th className="p-3 text-left md:text-center">Description</th>
+                  <th className="p-3 text-left md:text-center">From</th>
+                  <th className="p-3 text-left md:text-center">Order</th>
+                  <th className="p-3 text-left md:text-center">Amount</th>
+                </tr>
+              ))}
+            </thead></div>;
 };
 
 export default ManageEarningsTable;
