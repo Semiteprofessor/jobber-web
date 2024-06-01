702b81e3fe7311e8678522ac67171509dcf8189d
Taiwo Olapade <semiteprofessor@gmail.com>
2024-06-01 12:54:36 +0100

    Commit

--- src/features/chat/components/chatbox/ChatBox.tsx
+++ src/features/chat/components/chatbox/ChatBox.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { ChangeEvent, FC, FormEvent, ReactElement, RefObject, useRef, useState } from 'react';
+import { FaPaperPlane, FaTimes } from 'react-icons/fa';
+import Button from 'src/shared/button/Button';
+import TextInput from 'src/shared/inputs/TextInput';
+import { IResponse } from 'src/shared/shared.interface';
+import { generateRandomNumber, showErrorToast } from 'src/shared/utils/utils.service';
+import { useAppSelector } from 'src/store/store';
+import { IReduxState } from 'src/store/store.interface';
+import { v4 as uuidv4 } from 'uuid';
+
+import useChatScrollToBottom from '../../hooks/useChatScrollToBottom';
+import { IChatBoxProps, IConversationDocument, IMessageDocument } from '../../interfaces/chat.interface';
+import { useGetConversationQuery, useGetMessagesQuery, useSaveChatMessageMutation } from '../../services/chat.service';
+import ChatBoxSkeleton from './ChatBoxSkeleton';
 
 const ChatBox = () => {
-  return (
-    <div>ChatBox</div>
-  )
-}
+  return <div>ChatBox</div>;
+};
 
-export default ChatBox
+export default ChatBox;
