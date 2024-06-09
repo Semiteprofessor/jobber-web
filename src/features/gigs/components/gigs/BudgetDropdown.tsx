4d0859946a5e114ab112c6af0a3cc207d347540d
Taiwo Olapade <semiteprofessor@gmail.com>
2024-06-09 17:23:25 +0100

    Commit

---
+++ src/features/gigs/components/gigs/BudgetDropdown.tsx
@@ -0,0 +1,21 @@
+import { ChangeEvent, FC, KeyboardEvent, ReactElement, useState } from 'react';
+import { FaChevronDown, FaChevronUp, FaTimes } from 'react-icons/fa';
+import { useSearchParams } from 'react-router-dom';
+import { ISelectedBudget } from 'src/features/gigs/interfaces/gig.interface';
+import Button from 'src/shared/button/Button';
+import TextInput from 'src/shared/inputs/TextInput';
+import { saveToLocalStorage } from 'src/shared/utils/utils.service';
+
+const BudgetDropdown: FC = (): ReactElement => {
+  const [searchParams, setSearchParams] = useSearchParams({});
+  const [toggleDropdown, setToggleDropdown] = useState<boolean>(false);
+  const [selectedBudget, setSelectedBudget] = useState<ISelectedBudget>({ minPrice: '', maxPrice: '' });
+
+
+const BudgetDropdown = () => {
+  return (
+    <div>BudgetDropdown</div>
+  )
+}
+
+export default BudgetDropdown
--- src/features/gigs/components/gigs/Gigs.tsx
+++ src/features/gigs/components/gigs/Gigs.tsx
@@ -13,31 +13,7 @@ import {
   replaceDashWithSpaces,
   replaceSpacesWithDash,
   saveToLocalStorage
-} from 'src/shared/utils/utils.service';
-import { v4 as uuidv4 } from 'uuid';
-
-import { IGigsProps, ISellerGig } from '../../interfaces/gig.interface';
-import { useSearchGigsQuery } from '../../services/search.service';
-import BudgetDropdown from './components/BudgetDropdown';
-import DeliveryTimeDropdown from './components/DeliveryTimeDropdown';
-
-const ITEMS_PER_PAGE = 10;
-import { find } from 'lodash';
-import { FC, useRef, useState } from 'react';
-import { Location, useLocation, useParams, useSearchParams } from 'react-router-dom';
-import GigCardDisplayItem from 'src/shared/gigs/GigCardDisplayItem';
-import GigPaginate from 'src/shared/gigs/GigPaginate';
-import CircularPageLoader from 'src/shared/page-loader/CircularPageLoader';
-import PageMessage from 'src/shared/page-message/PageMessage';
-import {
-  categories,
-  getDataFromLocalStorage,
-  lowerCase,
-  replaceAmpersandAndDashWithSpace,
-  replaceDashWithSpaces,
-  replaceSpacesWithDash,
-  saveToLocalStorage
-} from 'src/shared/utils/utils.service';
+} from 'src/shared/utils/util.service';
 import { v4 as uuidv4 } from 'uuid';
 
 import { IGigsProps, ISellerGig } from '../../interfaces/gig.interface';
