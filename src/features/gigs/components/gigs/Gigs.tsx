ae0c7b4e9607c06d3b5db79c1e849deb87da6696
Taiwo Olapade <semiteprofessor@gmail.com>
2024-06-09 15:51:51 +0100

    Commit

--- src/features/gigs/components/gigs/Gigs.tsx
+++ src/features/gigs/components/gigs/Gigs.tsx
@@ -22,8 +22,60 @@ import BudgetDropdown from './components/BudgetDropdown';
 import DeliveryTimeDropdown from './components/DeliveryTimeDropdown';
 
 const ITEMS_PER_PAGE = 10;
+import { find } from 'lodash';
+import { FC, useRef, useState } from 'react';
+import { Location, useLocation, useParams, useSearchParams } from 'react-router-dom';
+import GigCardDisplayItem from 'src/shared/gigs/GigCardDisplayItem';
+import GigPaginate from 'src/shared/gigs/GigPaginate';
+import CircularPageLoader from 'src/shared/page-loader/CircularPageLoader';
+import PageMessage from 'src/shared/page-message/PageMessage';
+import {
+  categories,
+  getDataFromLocalStorage,
+  lowerCase,
+  replaceAmpersandAndDashWithSpace,
+  replaceDashWithSpaces,
+  replaceSpacesWithDash,
+  saveToLocalStorage
+} from 'src/shared/utils/utils.service';
+import { v4 as uuidv4 } from 'uuid';
 
-const Gigs = () => {
+import { IGigsProps, ISellerGig } from '../../interfaces/gig.interface';
+import { useSearchGigsQuery } from '../../services/search.service';
+import BudgetDropdown from './components/BudgetDropdown';
+import DeliveryTimeDropdown from './components/DeliveryTimeDropdown';
+
+const ITEMS_PER_PAGE = 10;
+
+const Gigs: FC<IGigsProps> = ({ type }) => {
+  const [itemFrom, setItemFrom] = useState<string>('0');
+  const [paginationType, setPaginationType] = useState<string>('forward');
+  const [searchParams] = useSearchParams();
+  const { category } = useParams<string>();
+  const location: Location = useLocation();
+  const updatedSearchParams: URLSearchParams = new URLSearchParams(searchParams.toString());
+  const queryType: string =
+    type === 'search'
+      ? replaceDashWithSpaces(`${updatedSearchParams}`)
+      : `query=${replaceAmpersandAndDashWithSpace(`${lowerCase(`${category}`)}`)}&${updatedSearchParams.toString()}`;
+  const { data, isSuccess, isLoading, isError } = useSearchGigsQuery({
+    query: `${queryType}`,
+    from: itemFrom,
+    size: `${ITEMS_PER_PAGE}`,
+    type: paginationType
+  });
+  const gigs = useRef<ISellerGig[]>([]);
+  let totalGigs = 0;
+  const filterApplied = getDataFromLocalStorage('filterApplied');
+  const categoryName = find(categories(), (item: string) => location.pathname.includes(replaceSpacesWithDash(`${lowerCase(`${item}`)}`)));
+  const gigCategories = categoryName ?? searchParams.get('query');
+
+  if (isSuccess) {
+    gigs.current = data.gigs as ISellerGig[];
+    totalGigs = data.total ?? 0;
+    saveToLocalStorage('filterApplied', JSON.stringify(false));
+  }
+
   return <div>Gigs</div>;
 };
 
