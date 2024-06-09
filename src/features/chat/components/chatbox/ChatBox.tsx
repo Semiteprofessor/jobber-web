7a76925d9bae26ff68c8e062dfbb4db0b4b24f14
Taiwo Olapade <semiteprofessor@gmail.com>
2024-06-09 12:58:06 +0100

    Commit

--- src/features/chat/components/chatbox/ChatBox.tsx
+++ src/features/chat/components/chatbox/ChatBox.tsx
@@ -3,7 +3,7 @@ import { FaPaperPlane, FaTimes } from 'react-icons/fa';
 import Button from 'src/shared/button/Button';
 import TextInput from 'src/shared/inputs/TextInput';
 import { IResponse } from 'src/shared/shared.interface';
-import { generateRandomNumber, showErrorToast } from 'src/shared/utils/utils.service';
+import { generateRandomNumber, showErrorToast } from 'src/shared/utils/util.service';
 import { useAppSelector } from 'src/store/store';
 import { IReduxState } from 'src/store/store.interface';
 import { v4 as uuidv4 } from 'uuid';
