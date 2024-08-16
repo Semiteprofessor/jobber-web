d28d64cee05377a42d605472986116c647929f3a
Taiwo Olapade <semiteprofessor@gmail.com>
2024-08-16 18:53:55 +0100

    Commit

--- src/features/order/components/Order.tsx
+++ src/features/order/components/Order.tsx
@@ -35,7 +35,30 @@ const Order: FC = (): ReactElement => {
     });
   }, [orderId]);
 
-  return <div>Order</div>;
+  return (
+    <div className="container mx-auto">
+      <div className="flex flex-wrap">
+        <div className="order-last w-full p-4 lg:order-first lg:w-2/3">
+          <OrderDetailsTable order={order} authUser={authUser} />
+          {order && order.buyerUsername === authUser.username && (
+            <div className="mt-4 flex flex-col justify-between bg-white md:flex-row">
+              <div className="flex w-full flex-col flex-wrap p-4 md:w-2/3">
+                <span className="text-base font-bold text-black lg:text-lg">
+                  {order.delivered ? 'Your delivery is here!' : 'Your delivery is now in the works'}
+                </span>
+                {order?.delivered ? (
+                  <p className="mt-1 w-5/6 flex-wrap text-sm">
+                    View the delivery to make sure you have exactly what you need. Let {order.sellerUsername} know your thoughts.
+                  </p>
+                ) : (
+                  <>
+                    <p className="mt-1 w-5/6 flex-wrap text-sm">We notified {order.sellerUsername} about your order.</p>
+                    <p className="mt-1 w-5/6 flex-wrap text-sm">
+                      You should receive your delivery by {TimeAgo.dayMonthYear(order.offer.newDeliveryDate)}
+                    </p>
+                  </>
+                )}
+              </div></div>;
 };
 
 export default Order;
