f7168c46d151bbd2b57f84c8c9076b0926876936
Taiwo Olapade <semiteprofessor@gmail.com>
2024-08-16 18:56:09 +0100

    Commit

--- src/features/order/components/DeliveryTimer.tsx
+++ src/features/order/components/DeliveryTimer.tsx
@@ -14,7 +14,37 @@ const DeliveryTimer: FC<IModalProps> = ({ order, authUser }): ReactElement => {
   });
   const [days, hours, minutes, seconds]: number[] = useCountDown(`${order?.offer.newDeliveryDate}`);
 
-  return <div>DeliveryTimer</div>;
+  return (
+    <>
+      {displayModal.extendDelivery && (
+        <ExtendDateModal order={order as IOrderDocument} onClose={() => setDisplayModal({ ...displayModal, extendDelivery: false })} />
+      )}
+      {displayModal.deliverWork && (
+        <DeliverWorkModal order={order as IOrderDocument} onClose={() => setDisplayModal({ ...displayModal, deliverWork: false })} />
+      )}
+      <div className="mb-6 flex flex-col gap-4 rounded-[4px] bg-white px-4 py-3">
+        <div className="text-base font-bold">
+          {!order?.delivered
+            ? `Time left ${authUser?.username === order?.sellerUsername ? 'to deliver' : 'for delivery'}`
+            : 'Want to deliver again?'}
+        </div>
+        {!order?.delivered && (
+          <div className="mb-1 flex justify-between text-center">
+            <div className="flex flex-col text-sm font-bold md:text-base">
+              {days} <span className="text-xs font-normal md:text-sm">days</span>
+            </div>
+            <div className="flex flex-col text-sm font-bold md:text-base">
+              {hours} <span className="text-xs font-normal md:text-sm">hours</span>
+            </div>
+            <div className="flex flex-col text-sm font-bold md:text-base">
+              {minutes} <span className="text-xs font-normal md:text-sm">minutes</span>
+            </div>
+            <div className="flex flex-col text-sm font-bold md:text-base">
+              {seconds} <span className="text-xs font-normal md:text-sm">seconds</span>
+            </div>
+          </div>
+        )}
+</div>;
 };
 
 export default DeliveryTimer;
