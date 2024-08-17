908ca6c076485122781ef0d99ce0e2b3e574c0f1
Taiwo Olapade <semiteprofessor@gmail.com>
2024-08-17 18:09:20 +0100

    Commit

--- src/features/order/components/Requirement.tsx
+++ src/features/order/components/Requirement.tsx
@@ -55,6 +55,55 @@ const Requirement: FC = (): ReactElement => {
     ]
   };
 
+  const startOrder = async (): Promise<void> => {
+    try {
+      const paymentIntentId = getDataFromLocalStorage('paymentIntentId');
+      const order: IOrderDocument = {
+        offer: {
+          gigTitle: offer.gigTitle,
+          price: offer.price,
+          description: offer.description,
+          deliveryInDays: offer.deliveryInDays,
+          oldDeliveryDate: offer.oldDeliveryDate,
+          newDeliveryDate: offer.newDeliveryDate,
+          accepted: true,
+          cancelled: offer.cancelled
+        },
+        gigId: `${gigId}`,
+        sellerId: `${gigRef?.current?.sellerId}`,
+        sellerImage: `${gigRef?.current?.profilePicture}`,
+        sellerUsername: `${gigRef?.current?.username}`,
+        sellerEmail: `${gigRef?.current?.email}`,
+        gigCoverImage: `${gigRef?.current?.coverImage}`,
+        gigMainTitle: `${gigRef?.current?.title}`,
+        gigBasicTitle: `${gigRef?.current?.basicTitle}`,
+        gigBasicDescription: `${gigRef?.current?.basicDescription}`,
+        buyerId: `${buyer._id}`,
+        buyerUsername: `${buyer.username}`,
+        buyerImage: `${buyer.profilePicture}`,
+        buyerEmail: `${buyer.email}`,
+        status: 'in progress',
+        orderId,
+        invoiceId,
+        quantity: 1,
+        dateOrdered: `${new Date()}`,
+        price: offer.price,
+        requirements: requirement,
+        paymentIntent: `${paymentIntentId}`,
+        events: {
+          placeOrder: order_date, // this should be the date after successful payment
+          requirements: `${new Date()}`,
+          orderStarted: `${new Date()}`
+        }
+      };
+      const response: IResponse = await createOrder(order).unwrap();
+      navigate(`/orders/${orderId}/activities`, { state: response?.order });
+      deleteFromLocalStorage('paymentIntent');
+    } catch (error) {
+      showErrorToast('Error starting your order.');
+    }
+  };
+
   return <div>Requirement</div>;
 };
 
