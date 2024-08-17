3ba988248f00eabb0330d8cb38788e7630840434
Taiwo Olapade <semiteprofessor@gmail.com>
2024-08-17 17:57:07 +0100

    Commit

--- src/features/order/components/Checkout.tsx
+++ src/features/order/components/Checkout.tsx
@@ -53,7 +53,49 @@ const Checkout: FC = (): ReactElement => {
             {/* <!-- CheckoutForm --> */}
           </div>
         </div>
-</div>;
+        <div className="w-full p-4 lg:w-1/3">
+          <div className="border border-grey mb-8">
+            <div className="pt-3 pb-4 px-4 mb-2 flex flex-col border-b md:flex-row">
+              <img className="object-cover w-20 h-11" src={state.coverImage} alt="Gig Cover Image" />
+              <h4 className="font-bold text-sm text-[#161c2d] mt-2 md:pl-4 md:mt-0">{state.title}</h4>
+            </div>
+            <ul className="list-none mb-0">
+              <li className="flex px-4 pt-1 pb-3 border-b border-grey">
+                <div className="font-normal text-sm">{state.description}</div>
+              </li>
+              <li className="flex justify-between px-4 pt-2 pb-2">
+                <div className="text-sm font-normal flex gap-2">
+                  <FaRegClock className="self-center" /> Expected delivery time
+                </div>
+                <span className="text-sm">
+                  {offer.deliveryInDays} day{offer.deliveryInDays > 1 ? 's' : ''}
+                </span>
+              </li>
+              <li className="flex justify-between px-4 pt-2 pb-2">
+                <div className="text-sm font-normal flex gap-2">
+                  <FaRegMoneyBillAlt className="self-center" /> Price
+                </div>
+                <span className="text-sm">${offer.price}</span>
+              </li>
+              <li className="flex justify-between px-4 pt-2 pb-2">
+                <div className="text-sm font-normal flex gap-2">
+                  <FaCog className="self-center" /> Service fee
+                </div>
+                <span className="text-sm">${serviceFee.toFixed(2)}</span>
+              </li>
+              <div className="border-b border-grey" />
+              <li className="flex justify-between px-4 py-4">
+                <div className="text-sm md:text-base font-semibold flex gap-2">
+                  <FaCog className="self-center" /> Total
+                </div>
+                <span className="text-sm md:text-base font-semibold">${offer.price + serviceFee}</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Checkout;
