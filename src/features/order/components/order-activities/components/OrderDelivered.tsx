e808c4efe35a096ad536549225a168f7c2c78107
Taiwo Olapade <semiteprofessor@gmail.com>
2024-08-23 23:16:49 +0100

    Commit

--- src/features/order/components/order-activities/components/OrderDelivered.tsx
+++ src/features/order/components/order-activities/components/OrderDelivered.tsx
@@ -13,7 +13,28 @@ import { TimeAgo } from 'src/shared/utils/timeago.utils';
 import { bytesToSize, downloadFile, getFileBlob, showErrorToast, showSuccessToast } from 'src/shared/utils/utils.service';
 import { v4 as uuidv4 } from 'uuid';
 
-const OrderDelivered = () => {
+const OrderDelivered: ForwardRefExoticComponent<Omit<IOrderDeliveredProps, 'ref'> & RefAttributes<HTMLDivElement>> = forwardRef(
+  (_, ref) => {
+    const { order, authUser, viewDeliveryBtnClicked } = useContext(OrderContext);
+    const [orderDeliveredModal, setOrderDeliveredModal] = useState<IOrderDeliveredModal>({
+      delivery: viewDeliveryBtnClicked as boolean,
+      deliveryApproval: false
+    });
+    const [approvalModalContent, setApprovalModalContent] = useState<IApprovalModalContent>();
+    const [showChatBox, setShowChatBox] = useState<boolean>(false);
+    const [approveOrder] = useApproveOrderMutation();
+    const chatSeller: IChatSellerProps = {
+      username: `${order?.sellerUsername}`,
+      _id: `${order?.sellerId}`,
+      profilePicture: `${order?.sellerImage}`,
+      responseTime: 1
+    };
+    const chatBuyer: IChatBuyerProps = {
+      username: `${order?.buyerUsername}`,
+      _id: `${order?.buyerId}`,
+      profilePicture: `${order?.buyerImage}`
+    };
+
   return <div>OrderDelivered</div>;
 };
 
