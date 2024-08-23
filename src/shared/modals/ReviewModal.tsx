6716b613d6ca03e93e9f0044e14b538ac04abe18
Taiwo Olapade <semiteprofessor@gmail.com>
2024-08-23 23:29:00 +0100

    Commit

--- src/shared/modals/ReviewModal.tsx
+++ src/shared/modals/ReviewModal.tsx
@@ -19,7 +19,43 @@ const LOADING_STATUS = {
   ERROR: 'error'
 };
 
-const ReviewModal = () => {
+const ReviewModal: FC<IModalProps> = ({ order, type, onClose }): ReactElement => {
+  const authUser = useAppSelector((state: IReduxState) => state.authUser);
+  const [review, setReview] = useState<string>('');
+  const [reviewRating, setReviewRating] = useState<number>(0);
+  const [status, setStatus] = useState<string>(LOADING_STATUS.IDLE);
+  const [addReview] = useAddReviewMutation();
+
+  const onAddReview = async (): Promise<void> => {
+    try {
+      setStatus(LOADING_STATUS.LOADING);
+      const reviewDocument: IReviewDocument = {
+        gigId: `${order?.gigId}`,
+        reviewerId: type === 'buyer-review' ? `${order?.buyerId}` : `${order?.sellerId}`,
+        sellerId: `${order?.sellerId}`,
+        reviewerImage: type === 'buyer-review' ? `${order?.buyerImage}` : `${order?.sellerImage}`,
+        review,
+        rating: reviewRating,
+        orderId: `${order?.orderId}`,
+        reviewType: type,
+        reviewerUsername: `${authUser?.username}`,
+        country: `${authUser?.username}`,
+        createdAt: `${new Date()}`
+      };
+      await addReview({ body: reviewDocument });
+      setStatus(LOADING_STATUS.SUCCESS);
+      showSuccessToast('Review added successfully.');
+      if (onClose) {
+        onClose();
+      }
+    } catch (error) {
+      setStatus(LOADING_STATUS.ERROR);
+      showErrorToast('Error adding review.');
+    }
+  };
+
+  const isLoading = status === LOADING_STATUS.LOADING;
+
   return <div>ReviewModal</div>;
 };
 
