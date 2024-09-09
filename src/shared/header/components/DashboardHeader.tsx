d6a70cf8f49a13f2fde2f495e3f0b189256dd56e
Taiwo Olapade <semiteprofessor@gmail.com>
2024-09-09 20:55:43 +0100

    Commit

--- src/shared/header/components/DashboardHeader.tsx
+++ src/shared/header/components/DashboardHeader.tsx
@@ -29,7 +29,8 @@ const DashboardHeader: FC = (): ReactElement => {
       setAuthUsername(`${username}`);
     });
   }, [authUser.username]);
-  return <>
+  return (
+    <>
       {openSidebar && <DashboardHeaderSideBar setOpenSidebar={setOpenSidebar} />}
       <header>
         <nav className="navbar peer-checked:navbar-active relative z-20 w-full border-b bg-white shadow-2xl shadow-gray-600/5 backdrop-blur dark:shadow-none">
@@ -55,7 +56,68 @@ const DashboardHeader: FC = (): ReactElement => {
                     </Link>
                   </div>
                 </div>
-              </div></>;
+              </div>
+              <div className="navmenu mb-16 hidden w-full cursor-pointer flex-wrap items-center justify-end space-y-8 rounded-3xl border border-gray-100 bg-white p-6 shadow-2xl shadow-gray-300/20 dark:border-gray-700 dark:bg-gray-800 dark:shadow-none md:flex-nowrap lg:m-0 lg:flex lg:w-6/12 lg:space-y-0 lg:border-0 lg:bg-transparent lg:p-0 lg:shadow-none">
+                <div className="text-[#74767e] lg:pr-4">
+                  <ul className="flex text-base font-medium">
+                    <li className="relative flex items-center">
+                      <Link to={`/${lowerCase(`${seller?.username}`)}/${seller?._id}/manage_orders`} className="px-3">
+                        <span>Orders</span>
+                      </Link>
+                    </li>
+                    <li className="relative flex items-center">
+                      <Link to={`/${lowerCase(`${seller?.username}`)}/${seller?._id}/manage_earnings`} className="px-3">
+                        <span>Earnings</span>
+                      </Link>
+                    </li>
+                    <li className="relative flex cursor-pointer items-center">
+                      <Button
+                        className="px-3 text-base font-medium"
+                        onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+                        label={
+                          <>
+                            <LazyLoadImage
+                              src={seller?.profilePicture}
+                              alt="profile image"
+                              className="h-7 w-7 rounded-full object-cover"
+                              placeholderSrc="https://placehold.co/330x220?text=Profile+Image"
+                              effect="blur"
+                            />
+                            {authUsername === authUser.username && (
+                              <span className="absolute bottom-1 left-8 h-2.5 w-2.5 rounded-full border-2 border-white bg-green-400"></span>
+                            )}
+                          </>
+                        }
+                      />
+                      <Transition
+                        show={isDropdownOpen}
+                        enter="transition ease-out duration-200"
+                        enterFrom="opacity-0 translate-y-1"
+                        enterTo="opacity-100 translate-y-0"
+                        leave="transition ease-in duration-150"
+                        leaveFrom="opacity-100 translate-y-0"
+                        leaveTo="opacity-0 translate-y-1"
+                      >
+                        <div className="absolute -right-48 z-50 mt-5 w-96">
+                          <SettingsDropdown
+                            seller={seller}
+                            buyer={buyer}
+                            authUser={authUser}
+                            type="seller"
+                            setIsDropdownOpen={setIsDropdownOpen}
+                          />
+                        </div>
+                      </Transition>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
 };
 
 export default DashboardHeader;
