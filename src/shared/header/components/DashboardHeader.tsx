8696e160bccef27496d6faba55b7f2cda1ae49fc
Taiwo Olapade <semiteprofessor@gmail.com>
2024-09-09 20:55:17 +0100

    Commit

--- src/shared/header/components/DashboardHeader.tsx
+++ src/shared/header/components/DashboardHeader.tsx
@@ -29,7 +29,33 @@ const DashboardHeader: FC = (): ReactElement => {
       setAuthUsername(`${username}`);
     });
   }, [authUser.username]);
-  return <div>DashboardHeader</div>;
+  return <>
+      {openSidebar && <DashboardHeaderSideBar setOpenSidebar={setOpenSidebar} />}
+      <header>
+        <nav className="navbar peer-checked:navbar-active relative z-20 w-full border-b bg-white shadow-2xl shadow-gray-600/5 backdrop-blur dark:shadow-none">
+          <div className="m-auto px-6 xl:container md:px-12 lg:px-6">
+            <div className="flex flex-wrap items-center justify-between gap-6 md:gap-0 md:py-3 lg:py-5">
+              <div className="flex w-full gap-x-4 lg:w-6/12">
+                <div className="flex w-full">
+                  <label htmlFor="hbr" className="peer-checked:hamburger relative z-20 -ml-4 block cursor-pointer p-6 lg:hidden">
+                    <Button
+                      className="m-auto flex h-0.5 w-5 items-center rounded transition duration-300"
+                      onClick={() => setOpenSidebar(!openSidebar)}
+                      label={
+                        <>{openSidebar ? <FaTimes className="h-6 w-6 text-sky-500" /> : <FaBars className="h-6 w-6 text-sky-500" />}</>
+                      }
+                    />
+                  </label>
+                  <div className="w-full gap-x-4 md:flex">
+                    <Link
+                      to={`/${lowerCase(`${seller?.username}`)}/${seller?._id}/seller_dashboard`}
+                      className="relative z-10 flex cursor-pointer justify-center self-center text-2xl font-semibold text-black lg:text-3xl"
+                    >
+                      Jobber
+                    </Link>
+                  </div>
+                </div>
+              </div></>;
 };
 
 export default DashboardHeader;
