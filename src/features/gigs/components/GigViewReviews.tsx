ed1b29be4df1062e2b0098c4697d4b3ac02df2d4
Taiwo Olapade <semiteprofessor@gmail.com>
2024-09-10 11:08:37 +0100

    Commit

--- src/features/gigs/components/GigViewReviews.tsx
+++ src/features/gigs/components/GigViewReviews.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { FC, ReactElement, useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import { GigContext } from 'src/features/gigs/context/GigContext';
+import { IGigViewReviewsProps } from 'src/features/gigs/interfaces/gig.interface';
+import { IRatingCategories, IRatingCategoryItem, IReviewDocument } from 'src/features/order/interfaces/review.interface';
+import { useGetReviewsByGigIdQuery } from 'src/features/order/services/review.service';
+import StarRating from 'src/shared/rating/StarRating';
+import { ratingTypes } from 'src/shared/utils/static-data';
+import { TimeAgo } from 'src/shared/utils/timeago.utils';
+import { rating, shortenLargeNumbers } from 'src/shared/utils/utils.service';
+import { v4 as uuidv4 } from 'uuid';
 
-const GigViewReviews = () => {
-  return (
-    <div>GigViewReviews</div>
-  )
-}
+const GigViewReviews: FC<IGigViewReviewsProps> = ({ showRatings, reviews, hasFetchedReviews }): ReactElement => {
+  const { gigId } = useParams<string>();
+  const { gig } = useContext(GigContext);
+  const { data, isSuccess } = useGetReviewsByGigIdQuery(`${gigId}`, { skip: hasFetchedReviews });
+  if (isSuccess && !hasFetchedReviews) {
+    reviews = data.reviews as IReviewDocument[];
+  }
 
-export default GigViewReviews
+  const percentage = (partialValue: number, totalValue: number): number => {
+    return (100 * partialValue) / totalValue;
+  };
+
+  return <div>GigViewReviews</div>;
+};
+
+export default GigViewReviews;
