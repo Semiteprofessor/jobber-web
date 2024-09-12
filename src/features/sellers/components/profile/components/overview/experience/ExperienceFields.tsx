e57264499478ccfcc472ed20ef7622f57582ce8d
Taiwo Olapade <semiteprofessor@gmail.com>
2024-09-12 10:50:09 +0100

    Commit

--- src/features/sellers/components/profile/components/overview/experience/ExperienceFields.tsx
+++ src/features/sellers/components/profile/components/overview/experience/ExperienceFields.tsx
@@ -64,7 +64,44 @@ const ExperienceFields: FC<IExperienceEditProps> = ({
     }
   };
 
-  return <div>ExperienceFields</div>;
+  return 
+  const onHandleUpdate = () => {
+    if (type === 'add') {
+      const item = {
+        title: experienceItem.title,
+        company: experienceItem.company,
+        startDate,
+        endDate,
+        description: experienceItem.description,
+        currentlyWorkingHere: experienceItem.currentlyWorkingHere
+      };
+      const clonedExperience: IExperience[] = cloneDeep(sellerProfile?.experience) as IExperience[];
+      clonedExperience.push(item);
+      if (setSellerProfile && setShowExperienceAddForm) {
+        setSellerProfile({ ...sellerProfile, experience: clonedExperience });
+        setShowExperienceAddForm(false);
+      }
+    } else {
+      const itemIndex: number = findIndex(sellerProfile?.experience, (value: IExperience) => value._id === selectedExperience?._id);
+      const clonedExperience: IExperience[] = cloneDeep(sellerProfile?.experience) as IExperience[];
+      const clonedItem: IExperience = {
+        _id: selectedExperience?._id,
+        title: experienceItem.title,
+        company: experienceItem.company,
+        startDate: `${startDate}`,
+        endDate: experienceItem.currentlyWorkingHere ? 'Present' : `${endDate}`,
+        description: experienceItem.description,
+        currentlyWorkingHere: experienceItem.currentlyWorkingHere
+      };
+      clonedExperience.splice(itemIndex, 1, clonedItem);
+      const filtered: IExperience[] = clonedExperience.filter((item: IExperience) => item.title !== '' && item.company !== '');
+      if (setSellerProfile && setShowExperienceEditForm) {
+        setSellerProfile({ ...sellerProfile, experience: filtered });
+        setShowExperienceEditForm(false);
+      }
+    }
+  };
+</div>;
 };
 
 export default ExperienceFields;
