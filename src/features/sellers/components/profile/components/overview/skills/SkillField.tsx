4daf5aeeb446f56d30fb64fc3192ef6fea043599
Taiwo Olapade <semiteprofessor@gmail.com>
2024-09-12 10:55:00 +0100

    Commit

--- src/features/sellers/components/profile/components/overview/skills/SkillField.tsx
+++ src/features/sellers/components/profile/components/overview/skills/SkillField.tsx
@@ -29,7 +29,42 @@ const SkillField: FC<ISkillEditProps> = ({ type, selectedSkill, setShowSkillAddF
     }
   };
 
-  return <div>SkillField</div>;
+  return (
+    <div className="flex w-full flex-col">
+      <div className="mb-6 px-3">
+        <TextInput
+          className="border-grey w-full rounded border p-2.5 text-sm font-normal text-gray-600 focus:outline-none"
+          placeholder="Skill E.g: Front End Developer"
+          type="text"
+          name="skill"
+          value={skill}
+          onChange={(event: ChangeEvent) => setSkill((event.target as HTMLInputElement).value)}
+        />
+      </div>
+      <div className="z-20 mx-3 my-2 flex cursor-pointer justify-start md:z-0 md:mt-0">
+        <Button
+          disabled={!skill && type === 'add'}
+          className={`md:text-md rounded bg-sky-500 px-6 py-1 text-center text-sm font-bold text-white hover:bg-sky-400 focus:outline-none md:py-2
+          ${!skill && type === 'add' ? 'cursor-not-allowed opacity-40' : 'cursor-pointer'}
+          `}
+          label={`${type === 'add' ? 'Add' : 'Update'}`}
+          onClick={onHandleUpdate}
+        />
+        &nbsp;&nbsp;
+        <Button
+          className="md:text-md rounded bg-gray-300 px-6 py-1 text-center text-sm font-bold hover:bg-gray-200 focus:outline-none md:py-2"
+          label="Cancel"
+          onClick={() => {
+            if (type === 'add' && setShowSkillAddForm) {
+              setShowSkillAddForm(false);
+            } else if (type === 'edit' && setShowSkillEditForm) {
+              setShowSkillEditForm(false);
+            }
+          }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default SkillField;
