ec181390c1a1d016eec8d96cfe4bd3a8b9df5f15
Taiwo Olapade <semiteprofessor@gmail.com>
2024-09-12 10:53:18 +0100

    Commit

--- src/features/sellers/components/profile/components/overview/skills/Skills.tsx
+++ src/features/sellers/components/profile/components/overview/skills/Skills.tsx
@@ -12,7 +12,24 @@ const Skills: FC = (): ReactElement => {
   const [selectedSkill, setSelectedSkill] = useState<string>('');
   const { sellerProfile, showEditIcons } = useContext(SellerContext);
 
-  return <div>Skills</div>;
+  return (
+    <div className="border-grey mt-6 border bg-white">
+      <div className="mb-1 flex justify-between border-b">
+        <h4 className="flex py-2.5 pl-3.5 text-sm font-bold text-[#161c2d] md:text-base">SKILLS</h4>
+        {showEditIcons && !showSkillAddForm && (
+          <span
+            onClick={() => {
+              setShowSkillAddForm(!showSkillAddForm);
+              setShowSkillEditForm(false);
+              setShowSkillEditIcon(false);
+              setSelectedSkill('');
+            }}
+            className="flex cursor-pointer items-center pr-3.5 text-sm text-[#00698c] md:text-base"
+          >
+            Add New
+          </span>
+        )}
+      </div></div>;
 };
 
 export default Skills;
