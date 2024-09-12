ac3c24d4d0b0b1f1f8ab86e43811508f45be283f
Taiwo Olapade <semiteprofessor@gmail.com>
2024-09-12 10:49:38 +0100

    Commit

--- src/features/sellers/components/profile/components/overview/experience/ExperienceFields.tsx
+++ src/features/sellers/components/profile/components/overview/experience/ExperienceFields.tsx
@@ -5,7 +5,7 @@ import { IExperience, IExperienceEditProps } from 'src/features/sellers/interfac
 import Button from 'src/shared/button/Button';
 import TextAreaInput from 'src/shared/inputs/TextAreaInput';
 import TextInput from 'src/shared/inputs/TextInput';
-import { yearsList } from 'src/shared/utils/utils.service';
+import { yearsList } from 'src/shared/utils/util.service';
 
 import Dropdown from '../../../../../../../shared/dropdown/Dropdown';
 
@@ -26,6 +26,43 @@ const ExperienceFields: FC<IExperienceEditProps> = ({
   });
   const [startDate, setStartDate] = useState<string>(selectedExperience?.startDate ?? 'Start Year');
   const [endDate, setEndDate] = useState<string>(selectedExperience?.endDate ?? 'End Year');
+
+  const onHandleUpdate = () => {
+    if (type === 'add') {
+      const item = {
+        title: experienceItem.title,
+        company: experienceItem.company,
+        startDate,
+        endDate,
+        description: experienceItem.description,
+        currentlyWorkingHere: experienceItem.currentlyWorkingHere
+      };
+      const clonedExperience: IExperience[] = cloneDeep(sellerProfile?.experience) as IExperience[];
+      clonedExperience.push(item);
+      if (setSellerProfile && setShowExperienceAddForm) {
+        setSellerProfile({ ...sellerProfile, experience: clonedExperience });
+        setShowExperienceAddForm(false);
+      }
+    } else {
+      const itemIndex: number = findIndex(sellerProfile?.experience, (value: IExperience) => value._id === selectedExperience?._id);
+      const clonedExperience: IExperience[] = cloneDeep(sellerProfile?.experience) as IExperience[];
+      const clonedItem: IExperience = {
+        _id: selectedExperience?._id,
+        title: experienceItem.title,
+        company: experienceItem.company,
+        startDate: `${startDate}`,
+        endDate: experienceItem.currentlyWorkingHere ? 'Present' : `${endDate}`,
+        description: experienceItem.description,
+        currentlyWorkingHere: experienceItem.currentlyWorkingHere
+      };
+      clonedExperience.splice(itemIndex, 1, clonedItem);
+      const filtered: IExperience[] = clonedExperience.filter((item: IExperience) => item.title !== '' && item.company !== '');
+      if (setSellerProfile && setShowExperienceEditForm) {
+        setSellerProfile({ ...sellerProfile, experience: filtered });
+        setShowExperienceEditForm(false);
+      }
+    }
+  };
 
   return <div>ExperienceFields</div>;
 };
