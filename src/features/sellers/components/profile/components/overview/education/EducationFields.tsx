4ce392aa18943919b92e3f79b5e3317906cef0a7
Taiwo Olapade <semiteprofessor@gmail.com>
2024-09-12 10:44:28 +0100

    Commit

--- src/features/sellers/components/profile/components/overview/education/EducationFields.tsx
+++ src/features/sellers/components/profile/components/overview/education/EducationFields.tsx
@@ -21,6 +21,41 @@ const EducationFields: FC<IEducationEditProps> = ({
   const [major, setMajor] = useState<string>(selectedEducation?.major ?? '');
   const [year, setYear] = useState<string>(selectedEducation?.year ?? 'Year');
 
+  const onHandleUpdate = (): void => {
+    if (type === 'add') {
+      const item: IEducation = {
+        title,
+        country,
+        university,
+        major,
+        year: `${year}`
+      };
+      const clonedEducation: IEducation[] = cloneDeep(sellerProfile?.education) as IEducation[];
+      clonedEducation.push(item);
+      if (setSellerProfile && setShowEducationAddForm) {
+        setSellerProfile({ ...sellerProfile, education: clonedEducation });
+        setShowEducationAddForm(false);
+      }
+    } else {
+      const itemIndex: number = findIndex(sellerProfile?.education, (value: IEducation) => value._id === selectedEducation?._id);
+      const clonedEducation: IEducation[] = cloneDeep(sellerProfile?.education) as IEducation[];
+      const clonedItem: IEducation = {
+        _id: selectedEducation?._id,
+        title,
+        country,
+        university,
+        major,
+        year
+      };
+      clonedEducation.splice(itemIndex, 1, clonedItem);
+      const filtered: IEducation[] = clonedEducation.filter((item: IEducation) => item.university !== '' && item.major !== '');
+      if (setSellerProfile && setShowEducationEditForm) {
+        setSellerProfile({ ...sellerProfile, education: filtered });
+        setShowEducationEditForm(false);
+      }
+    }
+  };
+
   return <div>EducationFields</div>;
 };
 
