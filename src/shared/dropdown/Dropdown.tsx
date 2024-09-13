0ab6a7717ec4968c401575f9175a5a35f0bf1484
Taiwo Olapade <semiteprofessor@gmail.com>
2024-09-13 13:26:58 +0100

    Commit

--- src/shared/dropdown/Dropdown.tsx
+++ src/shared/dropdown/Dropdown.tsx
@@ -37,7 +37,7 @@ const Dropdown: FC<IDropdownProps> = ({
     }
   };
 
-  return 
+  return (
     <div className={`w-full divide-y divide-gray-100 rounded border ${mainClassNames}`} style={style}>
       {(!showSearchInput || showSearchInput) && !toggleDropdown && (
         <Button
@@ -55,7 +55,45 @@ const Dropdown: FC<IDropdownProps> = ({
           onClick={() => setToggleDropdown(!toggleDropdown)}
         />
       )}
-    </div>;
+      {showSearchInput && toggleDropdown && (
+        <div className="flex">
+          <TextInput
+            type="text"
+            name="search"
+            value={inputText}
+            className="h-10 w-full items-center rounded pl-3 text-sm font-normal text-gray-600 focus:outline-none lg:text-base"
+            placeholder="Search..."
+            onChange={(event: ChangeEvent) => {
+              const inputValue: string = (event.target as HTMLInputElement).value;
+              setInputText(inputValue);
+              const filtered: string[] = filter(dropdownItems, (item: string) => item.toLowerCase().includes(inputValue.toLowerCase()));
+              setDropdownItems(filtered);
+              if (!inputValue) {
+                setDropdownItems(values);
+              }
+            }}
+          />
+          <div className="flex self-center" onClick={() => setToggleDropdown(!toggleDropdown)}>
+            <FaTimes className="mx-3 h-4 fill-current text-slate-900" />
+          </div>
+        </div>
+      )}
+
+      {toggleDropdown && (
+        <ul
+          className={`z-40 cursor-pointer overflow-y-scroll py-2 text-sm text-gray-700 dark:text-gray-200
+              ${dropdownClassNames}`}
+          style={{ maxHeight: `${maxHeight}px` }}
+        >
+          {dropdownItems.map((value: string) => (
+            <li key={uuidv4()} onClick={onHandleSelect}>
+              <div className="block px-4 py-2 text-slate-900 dark:hover:bg-gray-200">{value}</div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default Dropdown;
