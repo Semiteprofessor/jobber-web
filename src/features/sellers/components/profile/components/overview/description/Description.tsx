19b182dfac83e0168c3812c8b662eeb3884afd2c
Taiwo Olapade <semiteprofessor@gmail.com>
2024-09-13 10:36:34 +0100

    Commit

--- src/features/sellers/components/profile/components/overview/description/Description.tsx
+++ src/features/sellers/components/profile/components/overview/description/Description.tsx
@@ -8,7 +8,37 @@ const Description: FC = (): ReactElement => {
   const [showDescriptionEditForm, setShowDescriptionEditForm] = useState<boolean>(false);
   const [description, setDescription] = useState<string>(sellerProfile.description ? `${sellerProfile.description}` : '');
 
-  return <div>Description</div>;
+  return (
+    <div className="border-grey border bg-white">
+      <div className="mb-1 flex justify-between border-b">
+        <h4 className="flex py-2.5 pl-3.5 text-sm font-bold text-[#161c2d] md:text-base">DESCRIPTION</h4>
+        {showEditIcons && !showDescriptionEditForm && (
+          <span
+            onClick={() => {
+              setShowDescriptionEditForm(!showDescriptionEditForm);
+            }}
+            className="flex cursor-pointer items-center pr-3.5 text-sm text-[#00698c] md:text-base"
+          >
+            Edit Description
+          </span>
+        )}
+      </div>
+      <div className="mb-0 py-1.5">
+        {!showDescriptionEditForm && <div className="px-3.5 text-sm md:text-base">{sellerProfile.description}</div>}
+
+        {showDescriptionEditForm && (
+          <div className="flex w-full flex-col">
+            <div className="mb-4 px-3">
+              <TextAreaInput
+                className="border-grey focus:border-grey block w-full rounded border p-2.5 text-sm text-gray-900 focus:ring-blue-500"
+                placeholder="Write description..."
+                name="description"
+                value={description}
+                rows={5}
+                maxLength={600}
+                onChange={(event: ChangeEvent) => setDescription((event.target as HTMLInputElement).value)}
+              />
+            </div></div>;
 };
 
 export default Description;
