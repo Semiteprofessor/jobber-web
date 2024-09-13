3112530a6251de054d200f98a131fea130b511dc
Taiwo Olapade <semiteprofessor@gmail.com>
2024-09-13 13:14:51 +0100

    Commit

--- src/features/sellers/components/profile/components/overview/LanguageFields.tsx
+++ src/features/sellers/components/profile/components/overview/LanguageFields.tsx
@@ -17,6 +17,33 @@ const LanguageFields: FC<ILanguageEditFieldsProps> = ({
   const [level, setLevel] = useState<string>(selectedLanguage ? `${selectedLanguage.level}` : '');
   const [language, setLanguage] = useState<string>(selectedLanguage ? `${selectedLanguage.language}` : '');
 
+  const onHandleUpdate = (): void => {
+    if (type === 'add') {
+      const newItem: ILanguage = {
+        level,
+        language
+      };
+      const clonedLanguages: ILanguage[] = cloneDeep(sellerProfile?.languages) as ILanguage[];
+      clonedLanguages.push(newItem);
+      if (setSellerProfile && setShowLanguageAddForm) {
+        setSellerProfile({ ...sellerProfile, languages: clonedLanguages });
+        setShowLanguageAddForm(false);
+      }
+    } else {
+      const itemIndex: number = findIndex(sellerProfile.languages, (value: ILanguage) => value._id === selectedLanguage?._id);
+      const clonedItem: ILanguage = { level: !language ? '' : level, language, _id: selectedLanguage?._id };
+      const clonedLanguages: ILanguage[] = cloneDeep(sellerProfile?.languages) as ILanguage[];
+      clonedLanguages.splice(itemIndex, 1, clonedItem);
+      const filtered = filter(clonedLanguages, (item: ILanguage) => item.language !== '');
+      if (setSellerProfile && setShowLanguageEditForm && filtered.length > 0) {
+        setSellerProfile({ ...sellerProfile, languages: clonedLanguages });
+        setShowLanguageEditForm(false);
+      } else {
+        showErrorToast('You need to have at least one language.');
+      }
+    }
+  };
+
   return <div>LanguageFields</div>;
 };
 
