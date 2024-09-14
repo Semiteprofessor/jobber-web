be4a3bf798bfb567c52f0d64a563774e774c49a4
Taiwo Olapade <semiteprofessor@gmail.com>
2024-09-14 10:42:09 +0100

    Commit

--- src/features/sellers/components/profile/components/overview/education/Education.tsx
+++ src/features/sellers/components/profile/components/overview/education/Education.tsx
@@ -12,7 +12,28 @@ const Education: FC = (): ReactElement => {
   const [selectedEducation, setSelectedEducation] = useState<IEducation>();
   const { showEditIcons, sellerProfile } = useContext(SellerContext);
 
-  return <div>Education</div>;
+  return (
+    <div className="border-grey mt-6 border bg-white">
+      <div className="mb-1 flex justify-between border-b">
+        <h4 className="flex py-2.5 pl-3.5 text-sm font-bold text-[#161c2d] md:text-base">EDUCATION</h4>
+        {showEditIcons && !showEducationAddForm && (
+          <span
+            className="flex cursor-pointer items-center pr-3.5 text-sm text-[#00698c] md:text-base"
+            onClick={() => {
+              setShowEducationAddForm(!showEducationAddForm);
+              setShowEducationEditForm(false);
+            }}
+          >
+            Add New
+          </span>
+        )}
+      </div>
+      <ul className="mb-0 list-none pt-1.5">
+        {showEducationAddForm && (
+          <li className="flex justify-between">
+            <EducationFields type="add" setShowEducationAddForm={setShowEducationAddForm} />
+          </li>
+        )}</div>;
 };
 
 export default Education;
