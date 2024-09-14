5f471e6c8f9ce56cf3ef0ac8e040f73138e26348
Taiwo Olapade <semiteprofessor@gmail.com>
2024-09-14 10:41:18 +0100

    Commit

---
+++ src/features/sellers/components/profile/components/overview/education/Education.tsx
@@ -0,0 +1,18 @@
+import { FC, ReactElement, useContext, useState } from 'react';
+import { FaPencilAlt } from 'react-icons/fa';
+import { SellerContext } from 'src/features/sellers/context/SellerContext';
+import { IEducation } from 'src/features/sellers/interfaces/seller.interface';
+import { v4 as uuidv4 } from 'uuid';
+
+import EducationFields from './EducationFields';
+
+const Education: FC = (): ReactElement => {
+  const [showEducationAddForm, setShowEducationAddForm] = useState<boolean>(false);
+  const [showEducationEditForm, setShowEducationEditForm] = useState<boolean>(false);
+  const [selectedEducation, setSelectedEducation] = useState<IEducation>();
+  const { showEditIcons, sellerProfile } = useContext(SellerContext);
+
+  return <div>Education</div>;
+};
+
+export default Education;
