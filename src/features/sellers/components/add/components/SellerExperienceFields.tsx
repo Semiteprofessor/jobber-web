c4d98b28a48e38f39d2067916fb73da624a70bee
Taiwo Olapade <semiteprofessor@gmail.com>
2024-09-25 14:32:24 +0100

    Commit

---
+++ src/features/sellers/components/add/components/SellerExperienceFields.tsx
@@ -0,0 +1,13 @@
+import { ChangeEvent, FC, ReactElement } from 'react';
+import { IExperience, IExperienceProps } from 'src/features/sellers/interfaces/seller.interface';
+import Button from 'src/shared/button/Button';
+import Dropdown from 'src/shared/dropdown/Dropdown';
+import TextAreaInput from 'src/shared/inputs/TextAreaInput';
+import TextInput from 'src/shared/inputs/TextInput';
+import { yearsList } from 'src/shared/utils/utils.service';
+
+const SellerExperienceFields = () => {
+  return <div>SellerExperienceFields</div>;
+};
+
+export default SellerExperienceFields;
