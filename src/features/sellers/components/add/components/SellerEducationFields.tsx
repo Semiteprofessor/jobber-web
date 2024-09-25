a25b5b7838473bb5cb53aedc3e630d5c43afb8df
Taiwo Olapade <semiteprofessor@gmail.com>
2024-09-25 14:26:57 +0100

    Commit

---
+++ src/features/sellers/components/add/components/SellerEducationFields.tsx
@@ -0,0 +1,12 @@
+import { ChangeEvent, FC, ReactElement } from 'react';
+import { IEducation, IEducationProps } from 'src/features/sellers/interfaces/seller.interface';
+import Button from 'src/shared/button/Button';
+import Dropdown from 'src/shared/dropdown/Dropdown';
+import TextInput from 'src/shared/inputs/TextInput';
+import { countriesList, degreeList, yearsList } from 'src/shared/utils/util.service';
+
+const SellerEducationFields = () => {
+  return <div>SellerEducationFields</div>;
+};
+
+export default SellerEducationFields;
