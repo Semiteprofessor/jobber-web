db8f2f0f3210a738c395a793ba32355644df93bc
Taiwo Olapade <semiteprofessor@gmail.com>
2024-09-25 14:27:28 +0100

    Commit

--- src/features/sellers/components/add/components/SellerEducationFields.tsx
+++ src/features/sellers/components/add/components/SellerEducationFields.tsx
@@ -5,7 +5,29 @@ import Dropdown from 'src/shared/dropdown/Dropdown';
 import TextInput from 'src/shared/inputs/TextInput';
 import { countriesList, degreeList, yearsList } from 'src/shared/utils/util.service';
 
-const SellerEducationFields = () => {
+const SellerEducationFields: FC<IEducationProps> = ({ educationFields, setEducationFields }): ReactElement => {
+  const handleEducationFieldsChange = (event: ChangeEvent, index: number): void => {
+    if (setEducationFields && educationFields) {
+      const target: HTMLInputElement = event.target as HTMLInputElement;
+      const data: IEducation[] = [...educationFields];
+      data[index][target.name] = target.value;
+      setEducationFields([...data]);
+    }
+  };
+
+  const addEducationFields = (): void => {
+    const newfield: IEducation = {
+      country: 'Country',
+      university: '',
+      title: 'Title',
+      major: '',
+      year: 'Year'
+    };
+    if (setEducationFields && educationFields) {
+      setEducationFields([...educationFields, newfield]);
+    }
+  };
+
   return <div>SellerEducationFields</div>;
 };
 
