8750394e0384af7810151aeefc781e47d276738c
Taiwo Olapade <semiteprofessor@gmail.com>
2024-09-25 14:06:52 +0100

    Commit

---
+++ src/features/sellers/components/add/components/PersonalInfo.tsx
@@ -0,0 +1,4 @@
+import { ChangeEvent, FC, KeyboardEvent, ReactElement, useState } from 'react';
+import { IPersonalInfoProps } from 'src/features/sellers/interfaces/seller.interface';
+import TextAreaInput from 'src/shared/inputs/TextAreaInput';
+import TextInput from 'src/shared/inputs/TextInput';
