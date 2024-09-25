9205c9cdb34d68783996bd755a2d8d69ab33709c
Taiwo Olapade <semiteprofessor@gmail.com>
2024-09-25 14:34:15 +0100

    Commit

--- src/features/sellers/components/add/components/SellerExperienceFields.tsx
+++ src/features/sellers/components/add/components/SellerExperienceFields.tsx
@@ -6,7 +6,36 @@ import TextAreaInput from 'src/shared/inputs/TextAreaInput';
 import TextInput from 'src/shared/inputs/TextInput';
 import { yearsList } from 'src/shared/utils/utils.service';
 
-const SellerExperienceFields = () => {
+const SellerExperienceFields: FC<IExperienceProps> = ({ experienceFields, setExperienceFields }): ReactElement => {
+  const handleExperienceFieldsChange = (event: ChangeEvent, index: number): void => {
+    const target: HTMLInputElement = event.target as HTMLInputElement;
+    if (experienceFields && setExperienceFields) {
+      const data: IExperience[] = [...experienceFields];
+      if (target.name === 'currentlyWorkingHere') {
+        data[index]['currentlyWorkingHere'] = target.checked;
+        data[index]['endDate'] = target.checked ? '' : 'Present';
+        updatePresentEndDate(data, index);
+      } else {
+        data[index][target.name] = target.value;
+      }
+      setExperienceFields([...data]);
+    }
+  };
+
+  const addExperienceFields = (): void => {
+    const newField: IExperience = {
+      title: '',
+      company: '',
+      startDate: 'Start Year',
+      endDate: 'End Year',
+      currentlyWorkingHere: false,
+      description: ''
+    };
+    if (setExperienceFields && experienceFields) {
+      setExperienceFields([...experienceFields, newField]);
+    }
+  };
+
   return <div>SellerExperienceFields</div>;
 };
 
