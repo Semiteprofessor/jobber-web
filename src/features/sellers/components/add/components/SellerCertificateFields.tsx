7cd49bf68369fc55bb1ae33566896d87ac20b0b1
Taiwo Olapade <semiteprofessor@gmail.com>
2024-09-25 14:23:57 +0100

    Commit

--- src/features/sellers/components/add/components/SellerCertificateFields.tsx
+++ src/features/sellers/components/add/components/SellerCertificateFields.tsx
@@ -35,7 +35,64 @@ const SellerCertificateFields: FC<ICertificateProps> = ({ certificatesFields, se
     }
   };
 
-  return <div>SellerCertificateFields</div>;
+  return (
+    <div>
+      <div className="flex justify-between">
+        <h2 className="pb-4 text-xl font-bold">Awards/Certificates</h2>
+        <Button
+          className="md:text-md h-7 rounded bg-sky-500 px-6 text-center text-sm font-bold text-white hover:bg-sky-400 focus:outline-none md:px-8"
+          onClick={adCertificateFields}
+          label="Add More"
+        />
+      </div>
+      {certificatesFields?.map((input: ICertificate, index: number) => (
+        <div key={index}>
+          <div className="flex flex-col">
+            <TextInput
+              className="border-grey mb-4 w-full rounded border p-2.5 text-sm font-normal text-gray-600 focus:outline-none"
+              placeholder="Certificate or Award"
+              type="text"
+              name="name"
+              value={input.name}
+              onChange={(event: ChangeEvent) => handleCertificateFieldsChange(event, index)}
+            />
+            <TextInput
+              className="border-grey mb-4 w-full rounded border p-2.5 text-sm font-normal text-gray-600 focus:outline-none"
+              placeholder="Certificate From (e.g: Google)"
+              type="text"
+              name="from"
+              value={input.from}
+              onChange={(event: ChangeEvent) => handleCertificateFieldsChange(event, index)}
+            />
+          </div>
+          <div className="relative flex flex-col">
+            <Dropdown
+              text={`${input.year}`}
+              maxHeight="300"
+              mainClassNames="absolute bg-white z-10"
+              values={yearsList(100)}
+              onClick={(item: string) => {
+                const data: ICertificate[] = [...certificatesFields];
+                data[index]['year'] = `${item}`;
+                if (setCertificatesFields) {
+                  setCertificatesFields([...data]);
+                }
+              }}
+            />
+            {certificatesFields.length > 1 && index > 0 && (
+              <div className="mb-4 mt-16">
+                <Button
+                  className="md:text-md h-7 rounded bg-red-500 px-6 text-center text-sm font-bold text-white hover:bg-red-400 focus:outline-none md:px-8"
+                  onClick={() => removeCertificateFields(index)}
+                  label="Delete"
+                />
+              </div>
+            )}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default SellerCertificateFields;
