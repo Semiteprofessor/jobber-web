c5bef4d47e9a51832653191e79650a27fc9b2380
Taiwo Olapade <semiteprofessor@gmail.com>
2024-09-25 14:09:09 +0100

    Commit

--- src/features/sellers/components/add/components/PersonalInfo.tsx
+++ src/features/sellers/components/add/components/PersonalInfo.tsx
@@ -56,6 +56,59 @@ const PersonalInfo: FC<IPersonalInfoProps> = ({ personalInfo, setPersonalInfo })
           <span className="flex justify-end text-[#95979d] text-xs">{allowedInfoLength.oneliner} Characters</span>
         </div>
       </div>
+      <div className="grid md:grid-cols-5 mb-6">
+        <div className="text-base font-medium pb-2 mt-6 md:mt-0">
+          Oneliner<sup className="text-red-500 text-base top-[-0.3em]">*</sup>
+        </div>
+        <div className="w-full col-span-4">
+          <TextInput
+            className="w-full rounded border border-grey p-2.5 mb-1 text-sm font-normal text-gray-600 focus:outline-none"
+            type="text"
+            name="oneliner"
+            value={personalInfo.oneliner}
+            placeholder="E.g. Expert Mobile and Web Developer"
+            onChange={(event: ChangeEvent) => {
+              const onelinerValue: string = (event.target as HTMLInputElement).value;
+              setPersonalInfo({ ...personalInfo, oneliner: onelinerValue });
+              const counter: number = maxOneLinerCharacters - onelinerValue.length;
+              setAllowedInfoLength({ ...allowedInfoLength, oneliner: `${counter}/70` });
+            }}
+            onKeyDown={(event: KeyboardEvent) => {
+              const currentTextLength = (event.target as HTMLInputElement).value.length;
+              if (currentTextLength === maxOneLinerCharacters && event.key !== 'Backspace') {
+                event.preventDefault();
+              }
+            }}
+          />
+          <span className="flex justify-end text-[#95979d] text-xs">{allowedInfoLength.oneliner} Characters</span>
+        </div>
+      </div>
+      <div className="grid md:grid-cols-5 mb-6">
+        <div className="text-base font-medium pb-2">
+          Description<sup className="text-red-500 text-base top-[-0.3em]">*</sup>
+        </div>
+        <div className="w-full col-span-4">
+          <TextAreaInput
+            className="w-full rounded border border-grey p-2.5 mb-1 text-sm font-normal text-gray-600 focus:outline-none"
+            name="description"
+            value={personalInfo.description}
+            rows={5}
+            onChange={(event: ChangeEvent) => {
+              const descriptionValue: string = (event.target as HTMLInputElement).value;
+              setPersonalInfo({ ...personalInfo, description: descriptionValue });
+              const counter: number = maxDescriptionCharacters - descriptionValue.length;
+              setAllowedInfoLength({ ...allowedInfoLength, description: `${counter}/600` });
+            }}
+            onKeyDown={(event: KeyboardEvent) => {
+              const currentTextLength = (event.target as HTMLInputElement).value.length;
+              if (currentTextLength === maxDescriptionCharacters && event.key !== 'Backspace') {
+                event.preventDefault();
+              }
+            }}
+          />
+          <span className="flex justify-end text-[#95979d] text-xs">{allowedInfoLength.description} Characters</span>
+        </div>
+      </div>
     </div>
   );
 };
