2ca661f93878755aba33c83df5ade3b07b4b5915
Taiwo Olapade <semiteprofessor@gmail.com>
2024-09-25 14:08:10 +0100

    Commit

--- src/features/sellers/components/add/components/PersonalInfo.tsx
+++ src/features/sellers/components/add/components/PersonalInfo.tsx
@@ -2,3 +2,35 @@ import { ChangeEvent, FC, KeyboardEvent, ReactElement, useState } from 'react';
 import { IPersonalInfoProps } from 'src/features/sellers/interfaces/seller.interface';
 import TextAreaInput from 'src/shared/inputs/TextAreaInput';
 import TextInput from 'src/shared/inputs/TextInput';
+
+const PersonalInfo: FC<IPersonalInfoProps> = ({ personalInfo, setPersonalInfo }): ReactElement => {
+  const [allowedInfoLength, setAllowedInfoLength] = useState({
+    description: '600/600',
+    oneliner: '70/70'
+  });
+  const maxDescriptionCharacters = 600;
+  const maxOneLinerCharacters = 70;
+
+  return (
+    <div>
+      <div className="mb-6 grid md:grid-cols-5">
+        <div className="pb-2 text-base font-medium">
+          Fullname<sup className="top-[-0.3em] text-base text-red-500">*</sup>
+        </div>
+        <div className="col-span-4 w-full">
+          <TextInput
+            className="border-grey mb-1 w-full rounded border p-2.5 text-sm font-normal text-gray-600 focus:outline-none"
+            type="text"
+            name="fullname"
+            value={personalInfo.fullName}
+            onChange={(event: ChangeEvent) => {
+              setPersonalInfo({ ...personalInfo, fullName: (event.target as HTMLInputElement).value });
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PersonalInfo;
