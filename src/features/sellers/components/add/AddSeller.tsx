f3ba777dd139448eb9c62a0e520a10512b1c8598
Taiwo Olapade <semiteprofessor@gmail.com>
2024-09-25 14:01:27 +0100

    Commit

--- src/features/sellers/components/add/AddSeller.tsx
+++ src/features/sellers/components/add/AddSeller.tsx
@@ -7,7 +7,7 @@ import Breadcrumb from 'src/shared/breadcrumb/Breadcrumb';
 import Button from 'src/shared/button/Button';
 import CircularPageLoader from 'src/shared/page-loader/CircularPageLoader';
 import { IResponse } from 'src/shared/shared.interface';
-import { deleteFromLocalStorage, lowerCase, showErrorToast } from 'src/shared/utils/utils.service';
+import { deleteFromLocalStorage, lowerCase, showErrorToast } from 'src/shared/utils/util.service';
 import { useAppDispatch, useAppSelector } from 'src/store/store';
 import { IReduxState } from 'src/store/store.interface';
 
