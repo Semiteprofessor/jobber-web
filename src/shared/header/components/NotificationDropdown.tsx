0d746b6259e0288fc0e915c357af20c10a2b8f0f
Taiwo Olapade <semiteprofessor@gmail.com>
2025-03-14 16:00:06 +0100

    Commit

---
+++ src/shared/header/components/NotificationDropdown.tsx
@@ -0,0 +1,8 @@
+import React, { FC, ReactElement } from 'react';
+import { IHomeHeaderProps } from '../interfaces/header.interface';
+
+const NotificationDropdown: FC<IHomeHeaderProps> = ({ setIsNotificationDropdownOpen }): ReactElement => {
+  return <div>NotificationDropdown</div>;
+};
+
+export default NotificationDropdown;
