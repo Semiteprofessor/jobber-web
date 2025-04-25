df795bc7da90312a75cd9070e0eeec32bc6ca743
Taiwo Olapade <semiteprofessor@gmail.com>
2025-04-25 12:38:07 +0100

    Commit

--- src/shared/utils/image-utils.service.ts
+++ src/shared/utils/image-utils.service.ts
@@ -35,3 +35,59 @@ export const checkImage = (file: File, type: string): boolean => {
   }
   return isValid;
 };
+
+export const checkFile = (file: File): boolean => {
+  let hasError = false;
+  if (file.size > 1000000000) {
+    // 1 GB
+    window.alert('File is too large. Max. size is 1 GB.');
+    hasError = true;
+  }
+  if (!file.name.match(/\.(jpg|jpeg|png|gif|pdf|webp|zip|m4v|avi|mpg|mp4|webm)$/)) {
+    window.alert('Only files of type jpg, jpeg, png, gif or pdf are allowed');
+    hasError = true;
+  }
+  return hasError;
+};
+
+export const checkUrlExtension = (fileExtension: string): string => {
+  let fileType = '';
+  if (['jpg', 'jpeg', 'png', 'gif', 'webp'].includes(fileExtension)) {
+    fileType = 'image';
+  }
+
+  if (['pdf'].includes(fileExtension)) {
+    fileType = 'pdf';
+  }
+
+  if (['m4v', 'avi', 'mpg', 'mp4', 'webm'].includes(fileExtension)) {
+    fileType = 'video';
+  }
+
+  if (['zip'].includes(fileExtension)) {
+    fileType = 'zip';
+  }
+  return fileType;
+};
+
+export const fileType = (file: File): string => {
+  const list: string[] = file.name.split('.');
+  const fileType: string = list[list.length - 1];
+  return fileType;
+};
+
+export const readAsBase64 = (file: File): Promise<string | ArrayBuffer | null> => {
+  const reader: FileReader = new FileReader();
+  const fileValue: Promise<string | ArrayBuffer | null> = new Promise((resolve, reject) => {
+    reader.addEventListener('load', () => {
+      resolve(reader.result);
+    });
+
+    reader.addEventListener('error', (event: ProgressEvent<FileReader>) => {
+      reject(event);
+    });
+
+    reader.readAsDataURL(file);
+  });
+  return fileValue;
+};
