9b93bd425212a5a63b6e6ceb05f3a47dcf610b0b
Taiwo Olapade <semiteprofessor@gmail.com>
2025-04-25 14:03:26 +0100

    Commit

--- src/features/auth/components/ResetPassword.tsx
+++ src/features/auth/components/ResetPassword.tsx
@@ -1,7 +1,10 @@
-import { FC, ReactElement, useState } from 'react';
+import { FC, FormEvent, ReactElement, useState } from 'react';
 import { AUTH_FETCH_STATUS, IResetPassword } from '../interfaces/auth.interface';
 import { useAuthSchema } from 'src/shared/hooks/useAuthSchema';
 import { resetPasswordSchema } from '../schemes/auth.schema';
+import { useSearchParams } from 'react-router-dom';
+import { useResetPasswordMutation } from '../services/auth.service';
+import { IResponse } from 'src/shared/shared.interface';
 
 const ResetPassword: FC = (): ReactElement => {
   const [alertMessage, setAlertMessage] = useState<string>('');
@@ -10,7 +13,29 @@ const ResetPassword: FC = (): ReactElement => {
     confirmPassword: ''
   });
   const [status, setStatus] = useState<string>(AUTH_FETCH_STATUS.IDLE);
-  const [schemaValidation] = useAuthSchema({schema: resetPasswordSchema, userInfo});
+  const [schemaValidation] = useAuthSchema({ schema: resetPasswordSchema, userInfo });
+  const [searchParams] = useSearchParams({});
+  const [resetPassword, { isLoading }] = useResetPasswordMutation();
+
+  const onResetPassword = async (event: FormEvent): Promise<void> => {
+    event.preventDefault();
+    try {
+      const isValid: boolean = await schemaValidation();
+      if (isValid) {
+        const result: IResponse = await resetPassword({
+          password: userInfo.password,
+          confirmPassword: userInfo.confirmPassword,
+          token: `${searchParams.get('token')}`
+        }).unwrap();
+        setAlertMessage(`${result.message}`);
+        setStatus(AUTH_FETCH_STATUS.SUCCESS);
+        setUserInfo({ password: '', confirmPassword: '' });
+      }
+    } catch (error) {
+      setStatus(AUTH_FETCH_STATUS.ERROR);
+      setAlertMessage(error?.data.message);
+    }
+  };
   return <div>ResetPassword</div>;
 };
 
