6b99e8bd26cf03248ef75140d618adbfd24d718f
Taiwo Olapade <semiteprofessor@gmail.com>
2025-04-25 13:54:17 +0100

    Commit

--- src/features/auth/schemes/auth.schema.ts
+++ src/features/auth/schemes/auth.schema.ts
@@ -1,9 +1,15 @@
 import { object, ObjectSchema, string } from 'yup';
-import { ISignInPayload } from '../interfaces/auth.interface';
+import { ISignInPayload, ISignUpPayload } from '../interfaces/auth.interface';
 
 const loginUserSchema: ObjectSchema<ISignInPayload> = object({
   username: string().required({ username: 'Username is a required field' }).min(4, { username: 'Username is a required field' }),
   password: string().required({ password: 'Password is a required field' }).min(4, { Password: 'Username is a required field' }),
   browserName: string().optional().nullable().notRequired(),
-  deviceType: string().optional().nullable().notRequired(),
+  deviceType: string().optional().nullable().notRequired()
+});
+
+const registerUserSchema: ObjectSchema<ISignUpPayload> = object({
+  username: string().required({ username: 'Username is a required field' }).min(4, { username: 'Username is a required field' }),
+  username: string().required({ password: 'Password is a required field' }).min(4, { password: 'Password is a required field' }),
+  email: string().required({ email: 'Email is a required field' }).required({ email: 'Email is a required field' })
 });
