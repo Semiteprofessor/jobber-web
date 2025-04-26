c7d79fc18425f5906b0698e8cfd99f2531e57dbf
Taiwo Olapade <semiteprofessor@gmail.com>
2025-04-26 11:49:03 +0100

    Commit

--- src/shared/hooks/useAuthSchema.ts
+++ src/shared/hooks/useAuthSchema.ts
@@ -1,4 +1,21 @@
+import { useState } from 'react';
 import { IUseAuthSchema } from 'src/features/auth/interfaces/auth.interface';
 import { validationErrorsType } from 'src/shared/shared.interface';
 
-const useAuthSchema = ({ schema, userInfo }: IUseAuthSchema): [() => Promise<boolean>, validationErrorsType[]] => {};
+const useAuthSchema = ({ schema, userInfo }: IUseAuthSchema): [() => Promise<boolean>, validationErrorsType[]] => {
+  const [validationErrors, setValidationErrors] = useState<validationErrorsType[]>([]);
+
+  async function schemaValidation(): Promise<boolean> {
+    await schema
+      .validate(userInfo, { abortEarly: false })
+      .then(() => setValidationErrors([]))
+      .catch((err) => {
+        setValidationErrors([...err.errors]);
+      });
+    const validation: boolean = await schema.isValid(userInfo, { abortEarly: false });
+    return validation;
+  }
+  return [schemaValidation, validationErrors];
+};
+
+export { useAuthSchema };
