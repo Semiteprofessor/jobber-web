7a52ffc73a45569b0ebd34cf1e5af399dfe4509d
Taiwo Olapade <semiteprofessor@gmail.com>
2025-04-26 23:16:47 +0100

    Commit

--- src/features/index/Index.tsx
+++ src/features/index/Index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, lazy, LazyExoticComponent, ReactElement, Suspense } from 'react';
+import React, { FC, lazy, LazyExoticComponent, ReactElement, Suspense, useEffect } from 'react';
 import CircularPageLoader from 'src/shared/page-loader/CircularPageLoader';
 
 import Hero from './Hero';
@@ -6,10 +6,15 @@ import { IHeader } from 'src/shared/header/interfaces/header.interface';
 import GigTabs from 'src/features/index/gig-tabs/GigTabs';
 import HowItWorks from './HowItWorks';
 import Categories from './Categories';
+import { saveToSessionStorage } from 'src/shared/utils/util.service';
 
 const IndexHeader: LazyExoticComponent<FC<IHeader>> = lazy(() => import('src/shared/header/components/Header'));
 
 const Index: FC = (): ReactElement => {
+  useEffect(() => {
+    saveToSessionStorage(JSON.stringify(false), JSON.stringify(''));
+  }, []);
+
   return (
     <div className="flex flex-col">
       <Suspense fallback={<CircularPageLoader />}>
