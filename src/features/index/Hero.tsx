448ba749212b0a87e1fb190988bd28534c1b27a6
Taiwo Olapade <semiteprofessor@gmail.com>
2025-04-29 22:27:38 +0100

    Commit

--- src/features/index/Hero.tsx
+++ src/features/index/Hero.tsx
@@ -28,7 +28,22 @@ const Hero: FC = (): ReactElement => {
     };
   }, []);
 
-  return <div>Hero</div>;
+  return (
+    <div className="relative bg-white pb-20 pt-40 dark:bg-gray-900 lg:pt-44">
+      <div className="relative m-auto px-6 xl:container md:px-12 lg:px-6">
+        <h3 className="mb-4 mt-4 max-w-2xl pb-2 text-center text-2xl font-normal dark:text-white lg:text-left">
+          Expert categories: <span ref={typedElement}></span>
+        </h3>
+        <h1 className="text-center text-4xl font-black text-blue-900 dark:text-white sm:mx-auto sm:w-10/12 sm:text-5xl md:w-10/12 md:text-5xl lg:w-auto lg:text-left xl:text-7xl">
+          Hire expert freelancers <br className="hidden lg:block" />{' '}
+          <span className="relative bg-gradient-to-r from-blue-600 to-cyan-500 bg-clip-text text-transparent dark:from-blue-400 dark:to-cyan-300">
+            for your project
+          </span>
+          .
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
---
+++ src/features/index/Index.tsx
@@ -0,0 +1,9 @@
+import React, { Suspense } from 'react'
+
+const Index = () => {
+  return <div className="flex flex-col">
+    <Suspense fallback={<Cir}
+  </div>;
+}
+
+export default Index
