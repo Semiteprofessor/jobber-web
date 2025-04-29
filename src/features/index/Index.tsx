49ecd7271d5b562774f2a6dff2b8d5936f47ab31
Taiwo Olapade <semiteprofessor@gmail.com>
2025-04-29 22:37:05 +0100

    Commit

--- src/features/index/Index.tsx
+++ src/features/index/Index.tsx
@@ -1,9 +1,24 @@
-import React, { Suspense } from 'react'
+import React, { FC, lazy, LazyExoticComponent, ReactElement, Suspense } from 'react';
+import CircularPageLoader from 'src/shared/page-loader/CircularPageLoader';
 
-const Index = () => {
-  return <div className="flex flex-col">
-    <Suspense fallback={<Cir}
-  </div>;
-}
+import Categories from './Categories';
+import GigTabs from './gig-tabs/GigTabs';
+import Hero from './Hero';
+import HowItWorks from './HowItWorks';
+import { IHeader } from 'src/shared/header/interfaces/header.interface';
 
-export default Index
+const IndexHeader: LazyExoticComponent<FC<IHeader>> = lazy(() => import('src/shared/header/components/Header'));
+
+const Index: FC = (): ReactElement => {
+  return (
+    <div className="flex flex-col">
+      <Suspense fallback={<CircularPageLoader />}>
+        <IndexHeader navClass="navbar peer-checked:navbar-active fixed z-20 w-full border-b border-gray-100 bg-white/90 shadow-2xl shadow-gray-600/5 backdrop-blur dark:border-gray-800 dark:bg-gray-900/80 dark:shadow-none" />
+        <Hero />
+        <GigTabs />
+      </Suspense>
+    </div>
+  );
+};
+
+export default Index;
