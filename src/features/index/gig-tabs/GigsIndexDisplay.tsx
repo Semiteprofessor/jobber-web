5f64b920ec668fd10191d20d0212ac4679c41721
Taiwo Olapade <semiteprofessor@gmail.com>
2025-04-30 18:22:47 +0100

    Commit

--- src/features/index/gig-tabs/GigsIndexDisplay.tsx
+++ src/features/index/gig-tabs/GigsIndexDisplay.tsx
@@ -9,3 +9,19 @@ import CircularPageLoader from 'src/shared/page-loader/CircularPageLoader';
 import PageMessage from 'src/shared/page-message/PageMessage';
 import { categories, replaceAmpersandAndDashWithSpace, replaceDashWithSpaces, replaceSpacesWithDash } from 'src/shared/utils/utils.service';
 import { v4 as uuidv4 } from 'uuid';
+
+import GigIndexItem from './GigIndexItem';
+
+const ITEMS_PER_PAGE = 12;
+
+const GigsIndexDisplay: FC<IGigsProps> = ({ type }): ReactElement => {
+  const [itemFrom, setItemFrom] = useState<string>('0');
+  const [paginationType, setPaginationType] = useState<string>('forward');
+  const gigsCurrent = useRef<ISellerGig[]>([]);
+  const { category } = useParams<string>();
+  const [searchParams] = useSearchParams({});
+  const location = useLocation();
+  return <div>GigsIndexDisplay</div>;
+};
+
+export default GigsIndexDisplay;
