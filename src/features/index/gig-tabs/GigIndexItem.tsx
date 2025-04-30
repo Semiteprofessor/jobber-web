95560bb3336b108d96498e64d855ee5d4c60c985
Taiwo Olapade <semiteprofessor@gmail.com>
2025-04-30 17:57:59 +0100

    Commit

--- src/features/index/gig-tabs/GigIndexItem.tsx
+++ src/features/index/gig-tabs/GigIndexItem.tsx
@@ -1,9 +1,27 @@
 import React, { FC, ReactElement } from 'react';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import { Link } from 'react-router-dom';
 import { IGigsProps, ISellerGig } from 'src/features/gigs/interfaces/gig.interface';
+import { replaceSpacesWithDash } from 'src/shared/utils/util.service';
 
 const GigIndexItem: FC<IGigsProps> = ({ gig }): ReactElement => {
   const gigData: ISellerGig = gig as ISellerGig;
-  return <div>GigIndexItem</div>;
+  const title: string = replaceSpacesWithDash(gigData.title);
+  return (
+    <div>
+      <div>
+        <Link to={`/gig/${gigData.id}/${title}`}>
+          <LazyLoadImage
+            src={gigData.coverImage}
+            alt="Gig cover image"
+            className="w-full rounded-lg"
+            placeholderSrc="https://placehold.co/330x220?text=Profile+Image"
+            effect="blur"
+          />
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default GigIndexItem;
