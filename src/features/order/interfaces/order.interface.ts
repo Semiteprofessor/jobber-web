a382350b6b35f2daadb728f02c3d00dce8e8ed4d
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-05 00:00:50 +0100

    Commit

--- src/features/order/interfaces/order.interface.ts
+++ src/features/order/interfaces/order.interface.ts
@@ -44,3 +44,74 @@ export interface IOrderInvoice {
   buyerUsername: string;
   orderService: IOrderInvoiceService[];
 }
+
+export interface IOrderInvoiceService {
+  service: string;
+  quantity: number;
+  price: number;
+}
+
+export interface IOrderContext {
+  order?: IOrderDocument;
+  authUser?: IAuthUser;
+  orderInvoice?: IOrderInvoice;
+  viewDeliveryBtnClicked?: boolean;
+}
+
+export interface IExtendedDateModalProps {
+  order: IOrderDocument;
+  onClose: () => void;
+}
+
+export interface IOrderTableProps {
+  type: string;
+  orders: IOrderDocument[];
+  orderTypes: number;
+}
+
+export interface IActiveOrderProps {
+  activeOrders: IOrderDocument[];
+}
+
+export interface IOrderDisplayModal {
+  deliverWork: boolean;
+  extendDelivery: boolean;
+}
+
+export interface IOrderDeliveredModal {
+  delivery: boolean;
+  deliveryApproval: boolean;
+}
+
+export interface IOrderReviewModal {
+  buyerReview: boolean;
+  sellerReview: boolean;
+  buyerPanel: boolean;
+  sellerPanel: boolean;
+}
+
+export interface IExtendedDelivery {
+  originalDate: string;
+  newDate: string;
+  days: number;
+  reason: string;
+  deliveryDateUpdate?: string;
+}
+
+export interface IDeliveredWork {
+  message: string;
+  file: string;
+  fileType: string;
+  fileSize: number;
+  fileName: string;
+}
+
+export interface IOrderEvents {
+  placeOrder: string;
+  requirements: string;
+  orderStarted: string;
+  deliveryDateUpdate?: string;
+  orderDelivered?: string;
+  buyerReview?: string;
+  sellerReview?: string;
+}
