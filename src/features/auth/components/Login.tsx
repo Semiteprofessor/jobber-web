d0797dc5a50fc5e95e7fce54b87b58a9c31bd970
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-04 11:00:09 +0100

    Commit

--- src/features/auth/components/Login.tsx
+++ src/features/auth/components/Login.tsx
@@ -1,19 +1,46 @@
 import React, { FC, ReactElement } from "react";
 import { IHeaderModalProps } from "../../../shared/header/interfaces/header.interface";
 import ModalBg from "../../../shared/modals/ModalBg";
+import Button from "../../../shared/button/Button";
+import { FaTimes } from "react-icons/fa";
 
 const LoginModal: FC<IHeaderModalProps> = ({
   onClose,
   onToggle,
   onTogglePassword,
 }): ReactElement => {
+  const mobileOrientation = useMobileOrientation();
+  const deviceData = useDeviceData(window.navigator.userAgent);
+  const [alertMessage, setAlertMessage] = useState<string>("");
+  const [passwordType, setPasswordType] = useState<string>("password");
+  const [userInfo, setUserInfo] = useState<ISignInPayload>({
+    username: "",
+    password: "",
+    browserName: deviceData.browser.name,
+    deviceType: mobileOrientation.isLandscape ? "browser" : "mobile",
+  });
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const [schemaValidation] = useAuthSchema({
+    schema: loginUserSchema,
+    userInfo,
+  });
+  const [signIn, { isLoading }] = useSignInMutation();
   return (
     <ModalBg>
       <div className="relative top-[20%] mx-auto w-11/12 max-w-md rounded-lg bg-white md:w-2/3">
         <div className="relative px-5 py-5">
           <div className="mb-5 flex justify-between text-2xl font-bold text-gray-600">
             <h1 className="flex w-full justify-center">Sign In to Jobber</h1>
+            <Button
+              testId="closeModal"
+              className="cursor-pointer rounded text-gray-400 hover:text-gray-600"
+              role="button"
+              label={<FaTimes className="icon icon-tabler icon-tabler-x" />}
+              onClick={onClose}
+            />
           </div>
+          {alertMessage && <Alert />}
         </div>
       </div>
     </ModalBg>
