c07d9f25d2229b8fd9381914caea3a56b1734c15
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-04 10:56:35 +0100

    Commit

--- src/features/auth/components/Login.tsx
+++ src/features/auth/components/Login.tsx
@@ -1,12 +1,23 @@
 import React, { FC, ReactElement } from "react";
 import { IHeaderModalProps } from "../../../shared/header/interfaces/header.interface";
+import ModalBg from "../../../shared/modals/ModalBg";
 
 const LoginModal: FC<IHeaderModalProps> = ({
   onClose,
   onToggle,
   onTogglePassword,
 }): ReactElement => {
-  return (<ModalBg></ModalBg><div>LoginModal</div>);
+  return (
+    <ModalBg>
+      <div className="relative top-[20%] mx-auto w-11/12 max-w-md rounded-lg bg-white md:w-2/3">
+        <div className="relative px-5 py-5">
+          <div className="mb-5 flex justify-between text-2xl font-bold text-gray-600">
+            <h1 className="flex w-full justify-center">Sign In to Jobber</h1>
+          </div>
+        </div>
+      </div>
+    </ModalBg>
+  );
 };
 
 export default LoginModal;
