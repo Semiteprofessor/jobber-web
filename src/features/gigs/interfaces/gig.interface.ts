328ec8f24f35772a588deae25d7b8d4029ca27f2
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-06 23:56:12 +0100

    Commit

--- src/features/gigs/interfaces/gig.interface.ts
+++ src/features/gigs/interfaces/gig.interface.ts
@@ -39,3 +39,70 @@ export interface IGigDropdown {
   budget: boolean;
   deliveryTime: boolean;
 }
+
+export interface IShowGigModal {
+  image: boolean;
+  cancel: boolean;
+}
+
+export interface IGigTextLength {
+  gigTitle: number;
+  basicTitle: number;
+  basicDescription: number;
+  fullDescription: number;
+}
+
+export const GIG_MAX_LENGTH: IGigTextLength = {
+  gigTitle: 80,
+  basicTitle: 40,
+  basicDescription: 100,
+  fullDescription: 1200,
+};
+
+export interface IGigsProps {
+  type?: string;
+  gig?: ISellerGig;
+}
+
+export type GigType = string | string[] | number | unknown | undefined;
+
+export interface ICreateGig extends Record<string, GigType> {
+  // [key: string]: string | string[] | number | undefined;
+  sellerId?: string;
+  profilePicture?: string;
+  title: string;
+  categories: string;
+  description: string;
+  subCategories: string[];
+  tags: string[];
+  price: number;
+  coverImage: string;
+  expectedDelivery: string;
+  basicTitle: string;
+  basicDescription: string;
+}
+
+export interface ISellerGig {
+  _id?: string;
+  id?: string;
+  sellerId?: string;
+  title: string;
+  username?: string;
+  profilePicture?: string;
+  email?: string;
+  description: string;
+  active?: boolean;
+  categories: string;
+  subCategories: string[];
+  tags: string[];
+  ratingsCount?: number;
+  ratingSum?: number;
+  ratingCategories?: IRatingCategories;
+  expectedDelivery: string;
+  basicTitle: string;
+  basicDescription: string;
+  price: number;
+  coverImage: string;
+  createdAt?: Date | string;
+  sortId?: number;
+}
