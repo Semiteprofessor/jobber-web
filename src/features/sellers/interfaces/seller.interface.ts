1ef00b67ed25fe23beb35e8282a263ae8b5363e8
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-06 23:49:23 +0100

    Commit

--- src/features/sellers/interfaces/seller.interface.ts
+++ src/features/sellers/interfaces/seller.interface.ts
@@ -178,3 +178,63 @@ export interface IProfileTabsProps {
   type: string;
   setType?: Dispatch<SetStateAction<string>>;
 }
+
+export interface IShowEditItem {
+  fullname: boolean;
+  oneliner: boolean;
+}
+
+export interface ISellerProfileItem {
+  fullname: string;
+  oneliner: string;
+}
+
+// By extending ISellerDocument with the Record<string, any> you allow an object to contain other
+// string keys with any values along with those defined in the interface.
+// The nice part is that you still have the autocompletion for the defined properties
+export type SellerType =
+  | string
+  | string[]
+  | number
+  | IRatingCategories
+  | Date
+  | IExperience
+  | IExperience[]
+  | IEducation
+  | IEducation[]
+  | ICertificate
+  | ICertificate[]
+  | ILanguage
+  | ILanguage[]
+  | unknown
+  | undefined;
+
+export interface ISellerDocument extends Record<string, SellerType> {
+  _id?: string;
+  profilePublicId?: string;
+  fullName: string;
+  username?: string;
+  email?: string;
+  profilePicture?: string;
+  description: string;
+  country: string;
+  oneliner: string;
+  skills: string[];
+  ratingsCount?: number;
+  ratingSum?: number;
+  ratingCategories?: IRatingCategories;
+  languages: ILanguage[];
+  responseTime: number;
+  recentDelivery?: Date | string;
+  experience: IExperience[];
+  education: IEducation[];
+  socialLinks: string[];
+  certificates: ICertificate[];
+  ongoingJobs?: number;
+  completedJobs?: number;
+  cancelledJobs?: number;
+  totalEarnings?: number;
+  totalGigs?: number;
+  paypal?: string; // not needed
+  createdAt?: Date | string;
+}
