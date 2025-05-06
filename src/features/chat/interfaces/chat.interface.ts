409ed0243f0f99a4d47c09f691a6b53e29a3a583
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-06 23:53:16 +0100

    Commit

--- src/features/chat/interfaces/chat.interface.ts
+++ src/features/chat/interfaces/chat.interface.ts
@@ -26,3 +26,52 @@ export interface IConversationDocument {
   senderUsername: string;
   receiverUsername: string;
 }
+
+export interface IMessageDocument {
+  _id?: string;
+  conversationId?: string;
+  body?: string;
+  url?: string;
+  file?: string;
+  fileType?: string;
+  fileSize?: string;
+  fileName?: string;
+  gigId?: string;
+  sellerId?: string;
+  buyerId?: string;
+  senderUsername?: string;
+  senderPicture?: string;
+  receiverUsername?: string;
+  receiverPicture?: string;
+  isRead?: boolean;
+  hasOffer?: boolean;
+  offer?: IOffer;
+  hasConversationId?: boolean;
+  createdAt?: Date | string;
+}
+
+export interface IChatBoxProps {
+  seller: IChatSellerProps;
+  buyer: IChatBuyerProps;
+  gigId: string;
+  onClose: () => void;
+}
+
+export interface IChatSellerProps {
+  _id: string;
+  username: string;
+  profilePicture: string;
+  responseTime: number;
+}
+
+export interface IChatBuyerProps {
+  _id: string;
+  username: string;
+  profilePicture: string;
+}
+
+export interface IChatMessageProps {
+  message: IMessageDocument;
+  seller?: ISellerDocument;
+  gig?: ISellerGig;
+}
