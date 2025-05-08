9d538b1b56161fa07be46d3a30a4224cea41ed34
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-08 12:43:12 +0100

    Commit

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,5 +1,5 @@
 import { FC, ReactNode, Suspense } from 'react';
-import { RouteObject } from 'react-router-dom';
+import { RouteObject, useRoutes } from 'react-router-dom';
 import ResetPassword from './features/auth/components/ResetPassword';
 import AppPage from './features/AppPage';
 
@@ -22,9 +22,17 @@ const AppRouter: FC = () => {
           <ResetPassword />
         </Suspense>
       )
+    },
+    {
+      path: 'confirm_email',
+      element: (
+        <Suspense>
+          <ConfirmEmail />
+        </Suspense>
+      )
     }
   ];
-  return <div>AppRouter</div>;
+  return useRoutes(routes);
 };
 
 export default AppRouter;
---
+++ src/features/auth/components/ConfirmEmail.tsx
@@ -0,0 +1,7 @@
+import React, { FC, ReactElement } from 'react';
+
+const ConfirmEmail: FC = (): ReactElement => {
+  return <div>ConfirmEmail</div>;
+};
+
+export default ConfirmEmail;
