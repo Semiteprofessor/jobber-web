f518d5ae8e2924c1b3af715b9f03caaa7a815125
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-08 12:46:28 +0100

    Commit

--- src/features/auth/components/ConfirmEmail.tsx
+++ src/features/auth/components/ConfirmEmail.tsx
@@ -1,6 +1,34 @@
-import React, { FC, ReactElement } from 'react';
+import React, { FC, ReactElement, useCallback, useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { useAppDispatch } from 'src/store/store';
+import { useVerifyEmailMutation } from '../services/auth.service';
+import { AUTH_FETCH_STATUS } from '../interfaces/auth.interface';
+import { IResponse } from 'src/shared/shared.interface';
+import { addAuthUser } from '../reducers/auth.reducer';
 
 const ConfirmEmail: FC = (): ReactElement => {
+  const [alertMessage, setAlertMessage] = useState<string>('');
+  const [status, setStatus] = useState<string>(AUTH_FETCH_STATUS.IDLE);
+  const [searchParams] = useSearchParams({});
+  const dispatch = useAppDispatch();
+  const [verifyEmail] = useVerifyEmailMutation();
+
+  const onVerifyEmail = useCallback(async (): Promise<void> => {
+    try {
+      const result: IResponse = await verifyEmail(`${searchParams.get('v_token')}`).unwrap();
+      setAlertMessage('Email verified successfully.');
+      setStatus(AUTH_FETCH_STATUS.SUCCESS);
+      dispatch(addAuthUser({ authInfo: result.user }));
+    } catch (error) {
+      setStatus(AUTH_FETCH_STATUS.ERROR);
+      setAlertMessage(error?.data.message);
+    }
+  }, [dispatch, searchParams, verifyEmail]);
+
+  useEffect(() => {
+    onVerifyEmail();
+  }, [onVerifyEmail]);
+
   return <div>ConfirmEmail</div>;
 };
 
