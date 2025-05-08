46d3b907398a5711cfa2b79287652e9fc6b0d3fa
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-08 14:02:43 +0100

    Commit

--- src/features/auth/components/VerifyOTP.tsx
+++ src/features/auth/components/VerifyOTP.tsx
@@ -15,7 +15,7 @@ import { updateCategoryContainer } from 'src/shared/header/reducers/category.red
 import { updateHeader } from 'src/shared/header/reducers/header.reducer';
 import TextInput from 'src/shared/inputs/TextInput';
 import { IResponse } from 'src/shared/shared.interface';
-import { saveToSessionStorage, showErrorToast } from 'src/shared/utils/utils.service';
+import { saveToSessionStorage, showErrorToast } from 'src/shared/utils/util.service';
 import { socket } from 'src/sockets/socket.service';
 import { useAppDispatch, useAppSelector } from 'src/store/store';
 import { IReduxState } from 'src/store/store.interface';
@@ -27,6 +27,44 @@ import { useVerifyOTPMutation } from '../services/auth.service';
 const IndexHeader: LazyExoticComponent<FC<IHeader>> = lazy(() => import('src/shared/header/components/Header'));
 
 const VerifyOTP: FC = (): ReactElement => {
+  const authUser = useAppSelector((state: IReduxState) => state.authUser);
+  const showCategoryContainer = useAppSelector((state: IReduxState) => state.showCategoryContainer);
+  const mobileOrientation = useMobileOrientation();
+  const deviceData = useDeviceData(window.navigator.userAgent);
+  const [otp, setOTP] = useState<string>('');
+  const [hasLoaded, setHasLoaded] = useState<boolean>(false);
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const [verifyOTP, { isLoading }] = useVerifyOTPMutation();
+
+  const onVerifyOTP = async (): Promise<void> => {
+    try {
+      const data = {
+        otp,
+        browserName: deviceData.browser.name,
+        deviceType: mobileOrientation.isLandscape ? 'browser' : 'mobile'
+      };
+      const result: IResponse = await verifyOTP(data).unwrap();
+      dispatch(addAuthUser({ authInfo: result.user }));
+      const buyerResponse = await dispatch(buyerApi.endpoints.getCurrentBuyerByUsername.initiate() as unknown as Action);
+      dispatch(addBuyer(buyerResponse.data?.buyer));
+      const sellerResponse = await dispatch(
+        sellerApi.endpoints.getSellerByUsername.initiate(`${result.user?.username}`) as unknown as Action
+      );
+      dispatch(addSeller(sellerResponse.data?.seller));
+      dispatch(updateLogout(false));
+      dispatch(updateHeader('home'));
+      dispatch(updateCategoryContainer(true));
+      saveToSessionStorage(JSON.stringify(true), JSON.stringify(result.user?.username));
+      socket.emit('loggedInUsers', `${result?.user?.username}`);
+      setHasLoaded(true);
+      navigate('/');
+    } catch (error) {
+      setHasLoaded(false);
+      showErrorToast(error?.data?.message ?? 'Error verifying OTP');
+    }
+  };
+
   return <div>VerifyOTP</div>;
 };
 
