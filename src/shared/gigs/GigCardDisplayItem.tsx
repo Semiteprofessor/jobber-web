9e57ecdb4a264c0508cf807674e65ece7ac606ae
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-08 17:48:56 +0100

    Commit

--- src/shared/gigs/GigCardDisplayItem.tsx
+++ src/shared/gigs/GigCardDisplayItem.tsx
@@ -1,11 +1,12 @@
 import { FC, ReactElement, useEffect, useRef } from 'react';
 import { IGigCardItems, ISellerGig } from 'src/features/gigs/interfaces/gig.interface';
 import { useAppSelector } from 'src/store/store';
-import { replaceAmpersandAndDashWithSpace, replaceSpacesWithDash } from '../utils/util.service';
-import { NavigateFunction, useNavigate } from 'react-router-dom';
+import { lowerCase, replaceAmpersandAndDashWithSpace, replaceSpacesWithDash } from '../utils/util.service';
+import { Link, NavigateFunction, useNavigate } from 'react-router-dom';
 import { IReduxState } from 'src/store/store.interface';
 import { socket, socketService } from 'src/sockets/socket.service';
 import { find } from 'lodash';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 const GigCardDisplayItem: FC<IGigCardItems> = ({ gig, linkTarget, showEditIcon }): ReactElement => {
   const seller = useAppSelector((state: IReduxState) => state.seller);
@@ -33,7 +34,22 @@ const GigCardDisplayItem: FC<IGigCardItems> = ({ gig, linkTarget, showEditIcon }
     });
   }, [authUser.username, gig.username]);
 
-  return <div>GigCardDisplayItem</div>;
+  return (
+    <div className="rounded">
+      <div>
+        <Link to={`/gig/${lowerCase(`${gig.username}`)}/${title}/${gig.sellerId}/${gig.id}/view`} onClick={() => saveGigTitle(gig)}>
+          <LazyLoadImage
+            src={gig.coverImage}
+            alt="Gig cover image"
+            className="w-full rounded-lg"
+            wrapperClassName="bg-center"
+            placeholderSrc="https://placehold.co/330x220?text=Profile+Image"
+            effect="opacity"
+          />
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default GigCardDisplayItem;
