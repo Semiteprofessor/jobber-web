953cf708df8f0f290468778ba871c69dc3b370d5
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-10 10:17:23 +0100

    Commit

--- src/features/home/components/HomeSider.tsx
+++ src/features/home/components/HomeSider.tsx
@@ -1,12 +1,24 @@
-import React, { FC, ReactElement, useState } from 'react';
+import React, { FC, ReactElement, useEffect, useState } from 'react';
 import { ISliderState } from '../interfaces/home.interface';
-import { sliderImages } from 'src/shared/utils/static-data';
+import { sliderImages, sliderImagesText } from 'src/shared/utils/static-data';
+import { ISliderImagesText } from 'src/shared/shared.interface';
 
 const HomeSider: FC = (): ReactElement => {
   const [sliderState, setSiderState] = useState<ISliderState>({
     slideShow: sliderImages[0],
     slideIndex: 0
   });
+  const [sliderInterval, setSiderInterval] = useState<NodeJS.Timeout>();
+  const [currentSliderImageText, setCurrentSliderImageText] = useState<ISliderImagesText>(sliderImagesText[0]);
+
+  const { sliderIndex, slideShow } = sliderState;
+  let currentSlideIndex = 0;
+
+  useEffect(() => {
+    const timeInterval: NodeJS.Timeout = setInterval(() => {
+        autoMoveSlide()
+    })
+  })
   return <div>HomeSider</div>;
 };
 
