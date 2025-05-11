d3622bb2f0e803886ef7787c0b4e53be8f5ac7a9
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-11 10:48:29 +0100

    Commit

--- src/features/home/components/Home.tsx
+++ src/features/home/components/Home.tsx
@@ -1,8 +1,10 @@
 import React, { FC, ReactElement } from 'react';
 import HomeSlider from './HomeSlider';
+import { useAppSelector } from 'src/store/store';
+import { IReduxState } from 'src/store/store.interface';
 
 const Home: FC = (): ReactElement => {
-    const authUser = useAppSeector
+    const authUser = useAppSelector((state: IReduxState))
   return (
     <div className="m-auto px-6 w-screen relative min-h-screen xl:container md:px-12">
       <HomeSlider />
