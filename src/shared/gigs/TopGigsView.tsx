bbac96ea24ca5089f5df2de235970e25f530c4fb
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-11 15:31:28 +0100

    Commit

--- src/shared/gigs/TopGigsView.tsx
+++ src/shared/gigs/TopGigsView.tsx
@@ -1,7 +1,27 @@
-import React, { FC, ReactElement } from 'react';
+import React, { FC, ReactElement, useRef, useState } from 'react';
 import { IGigTopProps } from 'src/features/gigs/interfaces/gig.interface';
 
+interface IScrollProps {
+  start: boolean;
+  end: boolean;
+}
+
 const TopGigsView: FC<IGigTopProps> = ({ gigs, title, subTitle, category, width, type }): ReactElement => {
+  const navElement = useRef<HTMLDivElement | null>(null);
+  const [scroll, setScroll] = useState<IScrollProps>({
+    start: false,
+    end: false
+  });
+
+  const slideLeft = (): void => {
+    if (navElement.current) {
+      const maxScrollLeft = navElement.current.scrollWidth + navElement.current.clientWidth;
+      navElement.current.scrollLeft = navElement.current.scrollLeft < maxScrollLeft ? navElement.current.scrollLeft - 1000 : maxScrollLeft;
+      const maxWidth = navElement.current.scrollLeft + navElement.current.clientWidth;
+      setScroll({ start: maxWidth === navElement.current.scrollWidth, end: false });
+    }
+  };
+
   return <div>TopGigsView</div>;
 };
 
