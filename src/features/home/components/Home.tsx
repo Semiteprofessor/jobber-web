4c99240ee24e8118e0d5eb4fccd7dbcab6005d9d
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-11 12:55:28 +0100

    Commit

--- src/features/home/components/Home.tsx
+++ src/features/home/components/Home.tsx
@@ -2,16 +2,36 @@ import React, { FC, ReactElement } from 'react';
 import HomeSlider from './HomeSlider';
 import { useAppSelector } from 'src/store/store';
 import { IReduxState } from 'src/store/store.interface';
+import { useGetSellerByUsernameQuery } from 'src/features/sellers/services/seller.service';
+import { ISellerGig } from 'src/features/gigs/interfaces/gig.interface';
+import { ISellerDocument } from 'src/features/sellers/interfaces/seller.interface';
 
 const Home: FC = (): ReactElement => {
-    const authUser = useAppSelector((state: IReduxState) => state.authUser);
-    const {data, isSuccess} = useGetRandommSellerQuery('10')
+  const authUser = useAppSelector((state: IReduxState) => state.authUser);
+  const { data, isSuccess } = useGetSellerByUsernameQuery('10');
+  const { data, categoryData, isSuccess: isCategorySuccess } = useGetSellerByUsernameQuery(`${authUser.username}`);
+  const { data: topGigsData, isSuccess: isTopGigsSuccess } = useGetTopRatedGigsByCategoryQuery(`${authUser.username}`);
+
+  let sellers: ISellerDocument[] = [];
+  let categoryGigs: ISellerGig[] = [];
+  let topGigs: ISellerGig[] = [];
+
+  if (isSuccess) {
+    (sellers = data.sellers) as ISellerDocument[];
+  }
+
+  if (isCategorySuccess) {
+    categoryGigs = categoryData.gig as ISellerGig[];
+  }
+
+  if (isTopGigsSuccess) {
+    topGigs = topGigsData.gigs as ISellerGig[];
+  }
+
   return (
     <div className="m-auto px-6 w-screen relative min-h-screen xl:container md:px-12">
       <HomeSlider />
-      {
-        topGigs
-      }
+      {topGigs}
     </div>
   );
 };
