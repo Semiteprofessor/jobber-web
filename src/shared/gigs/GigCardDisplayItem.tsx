1a0a5019fe1baa5e5823dbc16deb9d2762bf1f6d
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-11 17:45:18 +0100

    Commit

--- src/shared/gigs/GigCardDisplayItem.tsx
+++ src/shared/gigs/GigCardDisplayItem.tsx
@@ -1,5 +1,11 @@
-import { FC, ReactElement } from 'react';
-import { IGigCardItems } from 'src/features/gigs/interfaces/gig.interface';
+import { FC, ReactElement, useEffect, useRef } from 'react';
+import { IGigCardItems, ISellerGig } from 'src/features/gigs/interfaces/gig.interface';
+import { useAppSelector } from 'src/store/store';
+import { replaceAmpersandAndDashWithSpace, replaceSpacesWithDash } from '../utils/util.service';
+import { NavigateFunction, useNavigate } from 'react-router-dom';
+import { IReduxState } from 'src/store/store.interface';
+import { socket, socketService } from 'src/sockets/socket.service';
+import { find } from 'lodash';
 
 const GigCardDisplayItem: FC<IGigCardItems> = ({ gig, linkTarget, showEditIcon }): ReactElement => {
   const seller = useAppSelector((state: IReduxState) => state.seller);
@@ -11,6 +17,21 @@ const GigCardDisplayItem: FC<IGigCardItems> = ({ gig, linkTarget, showEditIcon }
   const navigateToEditGig = (gigId: string): void => {
     navigate(`/manage_gigs/edit/${gigId}`, { state: gig });
   };
+
+  const saveGigTitle = (gig: ISellerGig): void => {
+    if (authUser?.username) {
+      const category: string = replaceAmpersandAndDashWithSpace(gig.categories);
+      socket.emit('category', category, authUser.username);
+    }
+  };
+
+  useEffect(() => {
+    socketService.setupSocketConnection();
+    socket.emit('getLoggedInUsers', '');
+    socket.on('online', (data: string[]) => {
+      sellerUsername.current = find(data, (name: string) => name === gig.username) as string;
+    });
+  }, [authUser.username, gig.username]);
 
   return <div>GigCardDisplayItem</div>;
 };
