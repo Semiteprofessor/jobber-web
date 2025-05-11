ba7b054da37c801349ccf311581fb506a5e155dd
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-11 12:58:03 +0100

    Commit

--- src/features/home/components/Home.tsx
+++ src/features/home/components/Home.tsx
@@ -1,15 +1,16 @@
-import React, { FC, ReactElement } from 'react';
+import React, { FC, ReactElement, useEffect } from 'react';
 import HomeSlider from './HomeSlider';
 import { useAppSelector } from 'src/store/store';
 import { IReduxState } from 'src/store/store.interface';
 import { useGetSellerByUsernameQuery } from 'src/features/sellers/services/seller.service';
 import { ISellerGig } from 'src/features/gigs/interfaces/gig.interface';
 import { ISellerDocument } from 'src/features/sellers/interfaces/seller.interface';
+import { useGetTopRatedGigsByCategoryQuery } from 'src/features/gigs/services/gigs.service';
 
 const Home: FC = (): ReactElement => {
   const authUser = useAppSelector((state: IReduxState) => state.authUser);
   const { data, isSuccess } = useGetSellerByUsernameQuery('10');
-  const { data, categoryData, isSuccess: isCategorySuccess } = useGetSellerByUsernameQuery(`${authUser.username}`);
+  const { data: categoryData, isSuccess: isCategorySuccess } = useGetSellerByUsernameQuery(`${authUser.username}`);
   const { data: topGigsData, isSuccess: isTopGigsSuccess } = useGetTopRatedGigsByCategoryQuery(`${authUser.username}`);
 
   let sellers: ISellerDocument[] = [];
@@ -27,6 +28,10 @@ const Home: FC = (): ReactElement => {
   if (isTopGigsSuccess) {
     topGigs = topGigsData.gigs as ISellerGig[];
   }
+
+  useEffect(() => {
+    socketService.setupSocketConnection();
+  })
 
   return (
     <div className="m-auto px-6 w-screen relative min-h-screen xl:container md:px-12">
