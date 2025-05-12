0bfc15adf51b399f77e2a609f9df7f64175588ea
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-12 15:41:30 +0100

    Commit

--- src/shared/header/components/HomeHeader.tsx
+++ src/shared/header/components/HomeHeader.tsx
@@ -148,7 +148,8 @@ const HomeHeader: FC<IHeaderModalProps> = ({ showCategotyContainer }): ReactElem
               text="Please verify your email before you proceed."
               onClick={onResendEmail}
             />
-          )}<div className="m-auto px-6 xl:container md:px-12 lg:px-6">
+          )}
+          <div className="m-auto px-6 xl:container md:px-12 lg:px-6">
             <div className="flex flex-wrap items-center justify-between gap-6 md:gap-0 md:py-3 lg:py-5">
               <div className="flex w-full gap-x-4 lg:w-6/12">
                 <div className="hidden w-full md:flex">
@@ -176,7 +177,8 @@ const HomeHeader: FC<IHeaderModalProps> = ({ showCategotyContainer }): ReactElem
                   </div>
                 </div>
                 <MobileHeaderSearchInput setOpenSidebar={setOpenSidebar} />
-              </div><div className="navmenu mb-16 hidden w-full cursor-pointer flex-wrap items-center justify-end space-y-8 rounded-3xl border border-gray-100 bg-white p-6 shadow-2xl md:flex-nowrap lg:m-0 lg:flex lg:w-6/12 lg:space-y-0 lg:border-0 lg:bg-transparent lg:p-0 lg:shadow-none">
+              </div>
+              <div className="navmenu mb-16 hidden w-full cursor-pointer flex-wrap items-center justify-end space-y-8 rounded-3xl border border-gray-100 bg-white p-6 shadow-2xl md:flex-nowrap lg:m-0 lg:flex lg:w-6/12 lg:space-y-0 lg:border-0 lg:bg-transparent lg:p-0 lg:shadow-none">
                 <div className="text-[#74767e] lg:pr-4">
                   <ul className="flex text-base font-medium">
                     <li className="relative z-50 flex cursor-pointer items-center">
@@ -206,7 +208,8 @@ const HomeHeader: FC<IHeaderModalProps> = ({ showCategotyContainer }): ReactElem
                           <NotificationDropdown setIsNotificationDropdownOpen={setIsNotificationDropdownOpen} />
                         </div>
                       </Transition>
-                    </li><li className="relative z-50 flex cursor-pointer items-center" onClick={toggleOrdersDropdown}>
+                    </li>
+                    <li className="relative z-50 flex cursor-pointer items-center" onClick={toggleOrdersDropdown}>
                       <Button
                         className="px-3"
                         label={
@@ -230,6 +233,56 @@ const HomeHeader: FC<IHeaderModalProps> = ({ showCategotyContainer }): ReactElem
                         </div>
                       </Transition>
                     </li>
+                    {buyer && !buyer.isSeller && (
+                      <li className="relative flex items-center">
+                        <Link
+                          to="/seller_onboarding"
+                          className="relative ml-auto flex h-9 items-center justify-center rounded-full bg-sky-500 text-white font-bold sm:px-6 hover:bg-sky-400"
+                        >
+                          <span>Become a Seller</span>
+                        </Link>
+                      </li>
+                    )}
+                    <li className="relative z-50 flex cursor-pointer items-center">
+                      <Button
+                        className="relative flex gap-2 px-3 text-base font-medium"
+                        onClick={toggleDropdown}
+                        label={
+                          <>
+                            <img src={`${authUser.profilePicture}`} alt="profile" className="h-7 w-7 rounded-full object-cover" />
+                            {authUsername === authUser.username && (
+                              <span className="absolute bottom-0 left-8 h-2.5 w-2.5 rounded-full border-2 border-white bg-green-400"></span>
+                            )}
+                            <span className="flex self-center">{authUser.username}</span>
+                          </>
+                        }
+                      />
+                      <Transition
+                        ref={settingsDropdownRef}
+                        show={isSettingsDropdown}
+                        enter="transition ease-out duration-200"
+                        enterFrom="opacity-0 translate-y-1"
+                        enterTo="opacity-100 translate-y-0"
+                        leave="transition ease-in duration-150"
+                        leaveFrom="opacity-100 translate-y-0"
+                        leaveTo="opacity-0 translate-y-1"
+                      >
+                        <div className="absolute -right-48 z-50 mt-5 w-96">
+                          <SettingsDropdown
+                            seller={seller}
+                            buyer={buyer}
+                            authUser={authUser}
+                            type="buyer"
+                            setIsDropdownOpen={setIsSettingsDropdown}
+                          />
+                        </div>
+                      </Transition>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
         </nav>
       </header>
     </>
