97c6c273ea0f39ce672105a928ef6ea4c33bca3f
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-13 15:15:55 +0100

    Commit

--- src/shared/header/components/mobile/HomeHeaderSidebar.tsx
+++ src/shared/header/components/mobile/HomeHeaderSidebar.tsx
@@ -1,7 +1,25 @@
-import React, { FC } from 'react';
-import { IHeaderSideBarProps } from '../../interfaces/header.interface';
+import React, { FC, ReactElement, useState } from 'react';
+import { IHeaderSideBarProps, ISettings } from '../../interfaces/header.interface';
+import { useAppDispatch, useAppSelector } from 'src/store/store';
+import { IReduxState } from 'src/store/store.interface';
+import { NavigateFunction, useNavigate } from 'react-router-dom';
+import { lowerCase } from 'src/shared/utils/util.service';
 
 const HomeHeaderSidebar: FC<IHeaderSideBarProps> = ({ setOpenSidebar }): ReactElement => {
+  const authUser = useAppSelector((state: IReduxState) => state.authUser);
+  const seller = useAppSelector((state: IReduxState) => state.seller);
+  const buyer = useAppSelector((state: IReduxState) => state.buyer);
+  const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+  const [toggleCategories, setToggleCategories] = useState<boolean>(false);
+  const dispatch = useAppDispatch();
+  const navigate: NavigateFunction = useNavigate();
+  const isSeller: boolean = (buyer && buyer.isSeller) as boolean;
+  const settings: ISettings[] = [
+    { id: 1, name: 'Add a new gig', url: `/manage_gigs/new/${seller?._id}`, show: isSeller },
+    { id: 2, name: 'Dashboard', url: `/users/${buyer?.username}/${buyer?._id}/orders`, show: true },
+    { id: 3, name: 'Profile', url: `/seller_profile/${lowerCase(`${seller?.username}`)}/${seller?._id}/edit`, show: isSeller },
+    { id: 4, name: 'Settings', url: `/${lowerCase(`${seller?.username}`)}/edit`, show: true }
+  ];
   return <div>HomeHeaderSidebar</div>;
 };
 
