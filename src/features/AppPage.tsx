9505c1bc584ae882bb3e445b303933c4e21b15a3
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-13 14:46:47 +0100

    Commit

--- src/features/AppPage.tsx
+++ src/features/AppPage.tsx
@@ -1,10 +1,15 @@
-import { useState } from "react";
-import { NavigateFunction, useNavigate } from "react-router-dom";
-import { useAppDispatch, useAppSelector } from "src/store/store";
-import { useCheckCurrentUserQuery } from "./auth/services/auth.service";
-import { useGetSellerByUsernameQuery } from "./sellers/services/seller.service";
-import { IReduxState } from "src/store/store.interface";
-
+import { useCallback, useState } from 'react';
+import { NavigateFunction, useNavigate } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from 'src/store/store';
+import { useCheckCurrentUserQuery } from './auth/services/auth.service';
+import { useGetSellerByUsernameQuery } from './sellers/services/seller.service';
+import { IReduxState } from 'src/store/store.interface';
+import { useGetCurrentBuyerByUsernameQuery } from './buyer/services/buyer.service';
+import { addAuthUser } from './auth/reducers/auth.reducer';
+import { addBuyer } from './buyer/reducers/buyer.reducer';
+import { addSeller } from './sellers/reducers/seller.reducer';
+import { getDataFromLocalStorage, saveToSessionStorage } from 'src/shared/utils/util.service';
+import { socket } from 'src/sockets/socket.service';
 
 const AppPage = () => {
   const authUser = useAppSelector((state: IReduxState) => state.authUser);
@@ -18,6 +23,28 @@ const AppPage = () => {
   const { data: sellerData, isLoading: isSellerLoading } = useGetSellerByUsernameQuery(`${authUser.username}`, {
     skip: authUser.id === null
   });
+
+  const checkUser = useCallback(async () => {
+    try {
+      if (currentUserData && currentUserData.user && !appLogout) {
+        setTokenIsValid(true);
+        dispatch(addAuthUser({ authInfo: currentUserData.user }));
+        dispatch(addBuyer(buyerData?.buyer));
+        dispatch(addSeller(sellerData?.seller));
+        saveToSessionStorage(JSON.stringify(true), JSON.stringify(authUser.username));
+        const becomeASeller = getDataFromLocalStorage('becomeASeller');
+        if (becomeASeller) {
+          navigate('/seller_onboarding');
+        }
+        if (authUser.username !== null) {
+          socket.emit('loggedInUsers', authUser.username);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, [currentUserData, navigate, dispatch, appLogout, authUser.username, buyerData, sellerData]);
+
   return <div>AppPage</div>;
 };
 
