efc7992e55b7a8e6dfc9f9a1ed1cf8d6b3a73f94
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-14 11:20:49 +0100

    Commit

--- src/shared/header/components/SettingsDropdown.tsx
+++ src/shared/header/components/SettingsDropdown.tsx
@@ -33,6 +33,58 @@ const SettingsDropdown: FC<IHomeHeaderProps> = ({ seller, authUser, buyer, type,
               {type === 'buyer' ? 'Switch to Selling' : 'Switch to Buying'}
             </Link>
           </li>
+        )} className="border-grey w-44 divide-y divide-gray-100 rounded border bg-white shadow-md">
+      <ul className="text-gray-700s py-2 text-sm" aria-labelledby="avatarButton">
+        {buyer && buyer.isSeller && (
+          <li className="mx-3 mb-1">
+            <Link
+              to={`${type === 'buyer' ? `/${lowerCase(`${authUser?.username}`)}/${seller?._id}/seller_dashboard` : '/'}`}
+              onClick={() => {
+                if (setIsDropdownOpen) {
+                  setIsDropdownOpen(false);
+                }
+                dispatch(updateHeader('sellerDashboard'));
+                dispatch(updateCategoryContainer(true));
+              }}
+              className="block w-full cursor-pointer rounded bg-sky-500 px-4s py-2 text-center font-bold text-white hover:bg-sky-400 focus:outline-none"
+            >
+              {type === 'buyer' ? 'Switch to Selling' : 'Switch to Buying'}
+            </Link>
+          </li>
+        )}
+        {buyer && buyer.isSeller && type === 'buyer' && (
+          <li>
+            <Link
+              to={`/manage_gigs/new/${seller?._id}`}
+              className="block px-4 py-2 hover:text-sky-400"
+              onClick={() => {
+                if (setIsDropdownOpen) {
+                  setIsDropdownOpen(false);
+                }
+                dispatch(updateHeader('home'));
+                dispatch(updateCategoryContainer(true));
+              }}
+            >
+              Add a new gig
+            </Link>
+          </li>
+        )}
+        {type === 'buyer' && (
+          <li>
+            <Link
+              to={`/users/${buyer?.username}/${buyer?._id}/orders`}
+              className="block px-4 py-2 hover:text-sky-400"
+              onClick={() => {
+                if (setIsDropdownOpen) {
+                  setIsDropdownOpen(false);
+                }
+                dispatch(updateHeader('home'));
+                dispatch(updateCategoryContainer(true));
+              }}
+            >
+              Dashboard
+            </Link>
+          </li>
         )}</div>;
 };
 
