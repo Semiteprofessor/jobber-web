2452ccd2202365f6ce69d6d69d10399473d3a6c2
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-14 11:13:24 +0100

    Commit

--- src/shared/header/components/OrderDropdown.tsx
+++ src/shared/header/components/OrderDropdown.tsx
@@ -2,7 +2,7 @@ import { FC, ReactElement } from 'react';
 import { FaEye } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import { IOrderDocument } from 'src/features/order/interfaces/order.interface';
-import { useGetOrdersByBuyerIdQuery } from 'src/features/order/services/';
+import { useGetOrdersByBuyerIdQuery } from 'src/features/order/services/order.service';
 import { lowerCase } from 'src/shared/utils/utils.service';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -15,7 +15,38 @@ const OrderDropdown: FC<IHomeHeaderProps> = ({ buyer, setIsDropdownOpen }): Reac
     orders = data.orders as IOrderDocument[];
   }
 
-  return <div>OrderDropdown</div>;
+  return (
+    <div className="border-grey z-20 flex max-h-[470px] flex-col justify-between rounded border bg-white shadow-md">
+      <div className="h-96 overflow-y-scroll">
+        {orders.length > 0 &&
+          orders.map((order: IOrderDocument) => (
+            <div key={uuidv4()} className="border-grey h-[76px] border-b pt-2 text-left hover:bg-gray-50">
+              <Link
+                to={`/orders/${order.orderId}/activities`}
+                className="flex px-4"
+                onClick={() => {
+                  if (setIsDropdownOpen) {
+                    setIsDropdownOpen(false);
+                  }
+                }}
+              >
+                <div className="mt-1 flex-shrink-0">
+                  <img className="h-14 w-20 object-cover" src={order.gigCoverImage} alt="" />
+                </div>
+                <div className="w-full pl-3">
+                  <div className="text-[13px] font-normal leading-normal">{order.gigBasicTitle}</div>
+                  <div className="flex gap-2 text-[11px]">
+                    <span className="font-normal text-[#b5b6ba]">by {order.sellerUsername}</span>
+                    <span className="font-normal">&#x2022;</span>
+                    <span className={`rounded text-white px-2 ${lowerCase(order.status.replace(/ /g, ''))}`}>{order.status}</span>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default OrderDropdown;
