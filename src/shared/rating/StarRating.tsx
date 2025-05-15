7fbece8701101c9ce1425ea01c84e1b60ae89991
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-15 12:05:55 +0100

    Commit

--- src/shared/rating/StarRating.tsx
+++ src/shared/rating/StarRating.tsx
@@ -5,6 +5,22 @@ import { v4 as uuidv4 } from 'uuid';
 import { IStarRatingProps } from '../shared.interface';
 
 const StarRating: FC<IStarRatingProps> = ({ value, size, setReviewRating }): ReactElement => {
+  const [numberOfStars] = useState<number[]>([...Array(5).keys()].map((index: number) => index + 1));
+  const [rating, setRating] = useState<number>(0);
+
+  useEffect(() => {
+    if (value) {
+      setRating(value);
+    }
+  }, [value]);
+
+  const handleClick = (index: number): void => {
+    if (!value && setReviewRating) {
+      setRating(index);
+      setReviewRating(index);
+    }
+  };
+
   return <div>StarRating</div>;
 };
 
