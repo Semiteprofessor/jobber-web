89e229d024505b6d46a392cd5f2a3a8ad61078d9
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-15 11:45:34 +0100

    Commit

--- src/shared/gigs/GigPaginate.tsx
+++ src/shared/gigs/GigPaginate.tsx
@@ -16,8 +16,8 @@ const GigPaginate: FC<IGigPaginateProps> = ({
   const paginationCount: number[] = [...Array(Math.ceil((totalGigs as number) / itemsPerPage)).keys()];
 
   return (
-    <div>
-      <ul>
+    <div className="flex w-full justify-center">
+      <ul className="flex gap-8">
         <div
           className={`cursor-pointer p-3 ${itemOffset - 1 > 0 ? 'rounded-full border border-sky-400' : 'cursor-not-allowed text-gray-400'}`}
           onClick={() => {
@@ -28,7 +28,30 @@ const GigPaginate: FC<IGigPaginateProps> = ({
               setItemFrom(`${firstItem.sortId}`);
             }
           }}
-        ><FaArrowLeft  className='flex self-center'/></div>
+        >
+          <FaArrowLeft className="flex self-center" />
+        </div>
+        {showNumbers &&
+          paginationCount.map((_, index: number) => (
+            <li
+              key={uuidv4()}
+              className={`cursor-pointer px-3 py-2 ${itemOffset === index + 1 ? 'border-b-2 border-black font-bold text-black' : ''}`}
+              onClick={() => {
+                const selectedPage = index + 1;
+                itemOffset += 1;
+                if (itemOffset < index + 1) {
+                  setPaginationType('forward');
+                  setItemFrom(`${selectedPage * itemsPerPage - itemsPerPage}`);
+                } else if (itemOffset > index + 1) {
+                  const selectedCount = selectedPage * itemsPerPage + 1;
+                  setPaginationType('backward');
+                  setItemFrom(`${selectedCount}`);
+                }
+              }}
+            >
+              {index + 1}
+            </li>
+          ))}
       </ul>
     </div>
   );
