cfbc2d98f06916bb1c0397632a02ae25cec09dee
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-15 12:02:31 +0100

    Commit

--- src/features/index/gig-tabs/GigInfoDisplay.tsx
+++ src/features/index/gig-tabs/GigInfoDisplay.tsx
@@ -35,7 +35,36 @@ const GigInfoDisplay: FC = (): ReactElement => {
           />
         </Suspense>
       )}
-      <div></div>
+      <div className="flex w-screen flex-col">
+        <Header navClass="navbar peer-checked:navbar-active relative z-20 w-full border-b border-gray-100 bg-white/90 shadow-2xl shadow-gray-600/5 backdrop-blur dark:border-gray-800 dark:bg-gray-900/80 dark:shadow-none" />
+      {
+      isLoading ? (
+        <CircularPageLoader />
+      ) : (
+        <div className="relative m-auto mt-8 min-h-screen w-screen px-6 xl:container md:px-12 lg:px-6">
+            <main className="max-w-8xl container mx-auto mt-8">
+              <h2 className="mb-4 px-4 text-xl font-bold text-[#404145] lg:text-3xl">{gig.current.title}</h2>
+              <div className="mb-4 flex flex-row gap-x-2 px-4">
+                <img className="flex h-8 w-8 self-center rounded-full object-cover" src={gig.current.profilePicture} alt="" />
+                <span className="flex self-center font-extrabold">{gig.current.username}</span>
+                {gig.current.ratingSum && gig.current.ratingsCount && gig.current.ratingSum >= 1 && gig.current.ratingsCount >= 1 ? (
+                  <>
+                    <span className="flex self-center">|</span>
+                    <div className="flex w-full gap-x-1 self-center">
+                      <div className="mt-1 w-20 gap-x-2">
+                        <StarRating value={rating(gig.current.ratingSum / gig.current.ratingsCount)} size={14} />
+                      </div>
+                      <div className="ml-2 mt-[2px] flex gap-1 text-sm">
+                        <span className="text-orange-400">{rating(gig.current.ratingSum / gig.current.ratingsCount)}</span>
+                        <span className="">({shortenLargeNumbers(gig.current.ratingsCount)})</span>
+                      </div>
+                    </div>
+                  </>
+                ) : (
+                  <></>
+                )}
+              </div>
+      )}</div>
     </>
   );
 };
