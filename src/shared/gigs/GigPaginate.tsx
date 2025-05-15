ab0c972d5f49c3c2f5c23bb6d716f84bdd14301d
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-15 11:44:35 +0100

    Commit

--- src/shared/gigs/GigPaginate.tsx
+++ src/shared/gigs/GigPaginate.tsx
@@ -1,5 +1,9 @@
-import React, { FC } from 'react';
-import { IGigPaginateProps } from 'src/features/gigs/interfaces/gig.interface';
+import React, { FC, ReactElement } from 'react';
+import { FaArrowLeft } from 'react-icons/fa';
+import { IGigPaginateProps, ISellerGig } from 'src/features/gigs/interfaces/gig.interface';
+import { v4 as uuidv4 } from 'uuid';
+
+let itemOffset = 1;
 
 const GigPaginate: FC<IGigPaginateProps> = ({
   gigs,
@@ -9,7 +13,25 @@ const GigPaginate: FC<IGigPaginateProps> = ({
   setItemFrom,
   setPaginationType
 }): ReactElement => {
-  return <div>GigPaginate</div>;
+  const paginationCount: number[] = [...Array(Math.ceil((totalGigs as number) / itemsPerPage)).keys()];
+
+  return (
+    <div>
+      <ul>
+        <div
+          className={`cursor-pointer p-3 ${itemOffset - 1 > 0 ? 'rounded-full border border-sky-400' : 'cursor-not-allowed text-gray-400'}`}
+          onClick={() => {
+            if (itemOffset - 1 > 0) {
+              itemOffset -= 1;
+              setPaginationType('backward');
+              const firstItem: ISellerGig = gigs[0];
+              setItemFrom(`${firstItem.sortId}`);
+            }
+          }}
+        ><FaArrowLeft  className='flex self-center'/></div>
+      </ul>
+    </div>
+  );
 };
 
 export default GigPaginate;
