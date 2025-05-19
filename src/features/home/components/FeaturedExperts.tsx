002a40d90f81eae32544f765c63fd0e836d888b7
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-19 15:00:37 +0100

    Commit

--- src/features/home/components/FeaturedExperts.tsx
+++ src/features/home/components/FeaturedExperts.tsx
@@ -2,7 +2,7 @@ import { FC, ReactElement } from 'react';
 import { Link } from 'react-router-dom';
 import { ISellerDocument } from 'src/features/sellers/interfaces/seller.interface';
 import StarRating from 'src/shared/rating/StarRating';
-import { lowerCase, rating } from 'src/shared/utils/utils.service';
+import { lowerCase, rating } from 'src/shared/utils/util.service';
 import { v4 as uuidv4 } from 'uuid';
 
 import { IFeaturedExpertProps } from '../interfaces/home.interface';
