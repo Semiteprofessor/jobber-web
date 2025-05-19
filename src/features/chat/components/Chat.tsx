de84f48b8dd3162af1effde8e0873824101ed305
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-19 12:21:38 +0100

    Commit

--- src/features/chat/components/Chat.tsx
+++ src/features/chat/components/Chat.tsx
@@ -24,7 +24,21 @@ const Chat: FC = (): ReactElement => {
     chatMessageReceived(`${conversationId}`, chatMessagesData, chatMessages.current, setChatMessagesData);
   }, [chatMessagesData, conversationId]);
 
-  return <div>Chat</div>;
+  return (
+    <div className="border-grey mx-2 my-5 flex max-h-[90%] flex-wrap border lg:container lg:mx-auto">
+      <div className="lg:border-grey relative w-full overflow-hidden lg:w-1/3 lg:border-r">
+        <ChatList />
+      </div>
+
+      <div className="relative hidden w-full overflow-hidden md:w-2/3 lg:flex">
+        {conversationId && chatMessagesData.length > 0 ? (
+          <ChatWindow setSkip={setSkip} chatMessages={chatMessagesData} isLoading={isLoading} isError={isError} />
+        ) : (
+          <div className="flex w-full items-center justify-center">Select a user to chat with.</div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
