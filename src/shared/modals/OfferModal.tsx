c38ec87f2d9ab542b23bda9ad5ea92273270c9eb
Taiwo Olapade <semiteprofessor@gmail.com>
2025-05-19 16:06:03 +0100

    Commit

--- src/shared/modals/OfferModal.tsx
+++ src/shared/modals/OfferModal.tsx
@@ -65,7 +65,40 @@ const OfferModal: FC<IModalProps> = ({ header, gigTitle, receiver, authUser, sin
   };
 
   return (
-    <div>OfferModal</div>
+    <ModalBg>
+      <div className="fixed bottom-0 left-0 right-0 top-0 z-[30] flex w-full items-center justify-center">
+        <div className="relative bottom-auto left-auto right-auto top-auto max-h-[90vh] min-w-[500px] bg-white p-4">
+          <div className="relative px-5 py-5">
+            <div className="flex justify-between text-2xl font-bold ">
+              <h1 className="flex w-full justify-center">{header}</h1>
+              <Button
+                onClick={cancelBtnHandler}
+                className="cursor-pointer rounded text-gray-400 hover:text-gray-600"
+                role="button"
+                label={<FaTimes className="" />}
+              />
+            </div>
+          </div>
+
+          <div className="relative mb-16 px-5">
+            <div className="py-4">
+              <label htmlFor="username" className="text-[20px] font-medium leading-tight tracking-normal">
+                {gigTitle}
+              </label>
+            </div>
+            <div>
+              <label htmlFor="description" className="text-sm font-bold leading-tight tracking-normal">
+                Description<sup className="top-[-0.1em] text-base text-red-500">*</sup>
+              </label>
+              <TextAreaInput
+                className="border-grey mb-1 w-full rounded border p-2.5 text-sm font-normal text-gray-600 focus:outline-none"
+                placeholder="Write a description..."
+                name="description"
+                value={offer.description}
+                rows={5}
+                onChange={(event: ChangeEvent) => setOffer({ ...offer, description: (event.target as HTMLInputElement).value })}
+              />
+            </div></div>
   )
 }
 
